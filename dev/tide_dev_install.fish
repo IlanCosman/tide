743becdafe2183f8abe9634d07ba19097ce2980e
Ilan Cosman <ilancosman@gmail.com>
2020-05-21 14:32:03 -0700

    Add branch install for dev

--- dev/tide_dev_install.fish
+++ dev/tide_dev_install.fish
@@ -1,4 +1,21 @@
-function local_install -a localRepo
+function tide_dev_install
+    argparse 'b/branch' 'l/local' -- $argv
+
+    if set -q _flag_branch
+        curl -s "https://raw.githubusercontent.com/IlanCosman/tide/$argv/install.fish" | source; and tide_install $argv
+        return $status
+    end
+
+    if set -q _flag_local
+        _local_install $argv
+        return $status
+    end
+
+    printf '%s' 'Please indicate local/branch'
+    return 1
+end
+
+function _local_install -a localRepo
     if source "$localRepo/install.fish"
         cp -r "$localRepo/conf.d" $__fish_config_dir
         cp -r "$localRepo/functions" $__fish_config_dir
