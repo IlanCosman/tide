a21c438a20dec5d7affb8e1c29fd41ec61b827c9
Ilan Cosman <ilancosman@gmail.com>
2020-05-23 10:27:46 -0700

    Use fish_git_prompt %s trick instead of string replace

--- functions/_tide_item_git_prompt.fish
+++ functions/_tide_item_git_prompt.fish
@@ -1,9 +1,3 @@
 function _tide_item_git_prompt
-    if set -l gitPrompt (fish_git_prompt)
-
-        set -l gitPrompt (string replace '(' '' $gitPrompt)
-        set -l gitPrompt (string replace ')' '' $gitPrompt)
-
-        printf '%s ' (string trim $gitPrompt)
-    end
+    fish_git_prompt '%s '
 end
