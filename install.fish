81cd3463205a8d202b314cfe97d9a1e178b4df10
Ilan Cosman <ilancosman@gmail.com>
2020-06-06 21:23:56 -0700

    Check if git is installed at start of installation

--- install.fish
+++ install.fish
@@ -1,4 +1,10 @@
 function tide_install
+    # If git isn't installed, there's nothing we can do
+    if not command -q git
+        printf '%s\n' 'Git must be installed to download Tide.'
+        return 1
+    end
+
     argparse 'l/local' 'd/dev' -- $argv
 
     set -l location $argv[1]
