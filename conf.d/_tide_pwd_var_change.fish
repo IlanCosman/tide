437aecfa890fcdc4dfb17d9bf9c2804c2a1b299f
Ilan Cosman <ilancosman@gmail.com>
2020-06-13 14:27:13 -0700

    Fix / not showing at start of pwd

--- conf.d/_tide_pwd_var_change.fish
+++ conf.d/_tide_pwd_var_change.fish
@@ -1,6 +1,6 @@
 function _tide_pwd_var_change -v PWD
     set -l preDirNumberPwd (string replace $HOME '~' $PWD)
-    set -l preDirNumberPwdSplit (string split --no-empty '/' $preDirNumberPwd)
+    set -l preDirNumberPwdSplit (string split '/' $preDirNumberPwd)
 
     set -g _tide_split_pwd $preDirNumberPwdSplit[(math -s0 -$tide_pwd_max_dirs)..-1]
     set -g _tide_pwd (string join '/' $_tide_split_pwd)
@@ -10,13 +10,17 @@ function _tide_pwd_var_change -v PWD
     set -g _tide_pwd_anchors
 
     if contains 'first' $tide_pwd_anchors
-        set -a _tide_pwd_anchors $_tide_split_pwd[1]
+        if test -n "$_tide_split_pwd[1]"
+            set -a _tide_pwd_anchors $_tide_split_pwd[1]
+        else
+            set -a _tide_pwd_anchors $_tide_split_pwd[2]
+        end
     end
     if contains 'last' $tide_pwd_anchors
         set -a _tide_pwd_anchors $_tide_split_pwd[-1]
     end
     if contains 'git' $tide_pwd_anchors
-        set -a _tide_pwd_anchors (string replace $HOME '~' $_tide_git_dir | string split --no-empty -r -m1 '/')[2]
+        set -a _tide_pwd_anchors (string replace $HOME '~' $_tide_git_dir | string split -r -m1 '/')[2]
     end
 
     _tide_pwd
