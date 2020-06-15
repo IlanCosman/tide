284313a0a05a4762176a53d25323a42152c4eb6b
Ilan Cosman <ilancosman@gmail.com>
2020-06-15 15:20:19 -0700

    Fix pwd bold bug in wizard

--- tide_theme/configure/prompt_items/_fake_tide_item_pwd.fish
+++ tide_theme/configure/prompt_items/_fake_tide_item_pwd.fish
@@ -1,19 +1,18 @@
 function _fake_tide_item_pwd
-    if set -q $fake_tide_pwd_anchors[1]
-        set_color -o
+    if set -q fake_tide_pwd_anchors[1]
+        set_color -o $fake_tide_pwd_color_anchors
+        printf '%s' '~'
+        set_color normal
+
+        set_color $fake_tide_pwd_color_dirs
+        printf '%s' '/'
+        set_color normal
+
+        set_color -o $fake_tide_pwd_color_anchors
+        printf '%s ' 'src'
+        set_color normal
+    else
+        set_color $fake_tide_pwd_color_dirs
+        printf '%s ' '~/src'
     end
-
-    set_color $fake_tide_pwd_color_dirs
-    printf '%s' '~'
-    set_color normal
-
-    set_color $fake_tide_pwd_color_dirs
-    printf '%s' '/'
-
-    if set -q $fake_tide_pwd_anchors[1]
-        set_color -o
-    end
-
-    set_color $fake_tide_pwd_color_dirs
-    printf '%s ' 'src'
 end
