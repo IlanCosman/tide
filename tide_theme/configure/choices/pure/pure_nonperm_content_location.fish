4a1a8f279656ad908d9a85ed93348114467f86a7
Ilan Cosman <ilancosman@gmail.com>
2020-06-18 10:52:25 -0700

    Fix pure_nonperm menu bug

--- tide_theme/configure/choices/pure/pure_nonperm_content_location.fish
+++ tide_theme/configure/choices/pure/pure_nonperm_content_location.fish
@@ -7,17 +7,18 @@ function pure_nonperm_content_location
 
     _option 2 'Right'
     set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'newline' 'prompt_char'
-    set -a fake_tide_right_prompt_items 'cmd_duration'
+    set -g fake_tide_right_prompt_items 'cmd_duration'
     _display_prompt
 
     _display_restart_and_quit
 
     switch (_menu 'Choice' 1/2/r/q)
         case 1
-            set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'prompt_char'
+            set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'cmd_duration' 'newline' 'prompt_char'
             _next_choice 'pure/pure_show_time'
         case 2
             set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'newline' 'prompt_char'
+            set -g fake_tide_right_prompt_items 'cmd_duration'
             _next_choice 'pure/pure_show_time'
         case r
             _begin
