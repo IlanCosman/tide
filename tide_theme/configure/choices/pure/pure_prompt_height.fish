50ef6e300222774bd08f951dc072c00636258e1e
Ilan Cosman <ilancosman@gmail.com>
2020-06-18 11:19:05 -0700

    Hotfix for pure_prompt_height

--- tide_theme/configure/choices/pure/pure_prompt_height.fish
+++ tide_theme/configure/choices/pure/pure_prompt_height.fish
@@ -1,22 +1,45 @@
 function pure_prompt_height
     _title 'Prompt Height?'
 
+    # This is a quickfix for me being a noob
+    if contains 'cmd_duration' $fake_tide_left_prompt_items
+        set hasCmdDuration true
+    else
+        set -e hasCmdDuration
+    end
+
     _option 1 'One line'
-    set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'prompt_char'
+    if set -q hasCmdDuration
+        set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'cmd_duration' 'prompt_char'
+    else
+        set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'prompt_char'
+    end
     _display_prompt
 
     _option 2 'Two lines'
-    set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'newline' 'prompt_char'
+    if set -q hasCmdDuration
+        set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'cmd_duration' 'newline' 'prompt_char'
+    else
+        set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'newline' 'prompt_char'
+    end
     _display_prompt
 
     _display_restart_and_quit
 
     switch (_menu 'Choice' 1/2/r/q)
         case 1
-            set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'prompt_char'
+            if set -q hasCmdDuration
+                set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'cmd_duration' 'prompt_char'
+            else
+                set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'prompt_char'
+            end
             _next_choice 'all/prompt_spacing'
         case 2
-            set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'newline' 'prompt_char'
+            if set -q hasCmdDuration
+                set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'cmd_duration' 'newline' 'prompt_char'
+            else
+                set -g fake_tide_left_prompt_items 'pwd' 'git_prompt' 'newline' 'prompt_char'
+            end
             _next_choice 'all/prompt_spacing'
         case r
             _begin
