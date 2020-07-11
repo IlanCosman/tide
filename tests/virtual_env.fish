d77ec252d4cb9c24535b08d692695c95b185c8c8
Ilan Cosman <ilancosman@gmail.com>
2020-07-10 22:18:47 -0700

    Add virtual_env unit test

---
+++ tests/virtual_env.fish
@@ -0,0 +1,23 @@
+@mesg '---------virtual_env---------'
+
+function _virtual_env
+    _tide_decolor (_tide_item_virtual_env)
+end
+
+set -l dir ~/python_project
+
+mkdir -p $dir
+cd $dir
+
+@test 'nothing' (_virtual_env) -z
+
+python -m venv .venv
+source .venv/bin/activate.fish
+
+set -lx tide_virtual_env_display projectName
+@test 'projectName' (_virtual_env) = 'python_project'
+
+set -lx tide_virtual_env_display venvName
+@test 'venvName' (_virtual_env) = '.venv'
+
+rm -r $dir
