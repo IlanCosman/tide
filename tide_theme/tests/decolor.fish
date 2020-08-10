673131a419a765d9436f8d02d9b485b4f9b7dfae
Ilan Cosman <ilancosman@gmail.com>
2020-08-09 17:43:51 -0700

    Add decolor test

---
+++ tide_theme/tests/decolor.fish
@@ -0,0 +1,10 @@
+@mesg '---------cmd_duration---------'
+
+set -lx decolorText 'Hello Foo'
+
+function _decolor -a color
+    _tide_decolor (set_color $color)$decolorText
+end
+
+@test 'Color' (_decolor red) = $decolorText
+@test 'Normal' (_decolor normal) = $decolorText
