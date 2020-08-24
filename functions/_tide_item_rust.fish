581013beffc7505c25375f4e99701a0e2705bd20
Ilan Cosman <ilancosman@gmail.com>
2020-08-24 16:56:02 -0700

    Speed up rust item, type is slow, command is fast

--- functions/_tide_item_rust.fish
+++ functions/_tide_item_rust.fish
@@ -1,5 +1,5 @@
 function _tide_item_rust
-    if type --quiet rustc && test -e Cargo.toml -o (count *.rs) -gt 0
+    if command --quiet rustc && test -e Cargo.toml -o (count *.rs) -gt 0
         set -l rustVersion (rustc --version | string split ' ')[2]
 
         if test "$tide_rust_verbose_version" = false
--- functions/_tide_item_virtual_env.fish
+++ functions/_tide_item_virtual_env.fish
@@ -1,8 +1,8 @@
 function _tide_item_virtual_env
     if set -q VIRTUAL_ENV
-        set_color $tide_virtual_env_color
-
         set -l splitVirtualEnv (string split '/' "$VIRTUAL_ENV")
+
+        set_color $tide_virtual_env_color
 
         if test "$tide_virtual_env_display_mode" = 'projectName'
             printf '%s' {$tide_virtual_env_icon}' ' $splitVirtualEnv[-2]
