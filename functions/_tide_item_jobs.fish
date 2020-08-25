4e2954666784ece451351f04c667eee6aa6818c7
Ilan Cosman <ilancosman@gmail.com>
2020-08-24 17:20:45 -0700

    Add number of jobs option (closes #5)

--- functions/_tide_item_jobs.fish
+++ functions/_tide_item_jobs.fish
@@ -1,6 +1,10 @@
 function _tide_item_jobs
-    if jobs --quiet
+    if jobs --quiet # Use jobs --quiet because its faster, and most people wont use verbose
+        if test "$tide_jobs_verbose" = true
+            set jobs_number (jobs --pid | count) # --pid because it's faste than just jobs
+        end
+
         set_color $tide_jobs_color
-        printf '%s' $tide_jobs_icon
+        printf '%s' $tide_jobs_icon ' '{$jobs_number}
     end
 end
