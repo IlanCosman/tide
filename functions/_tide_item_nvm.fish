43469e4be8cf7040298b58d1327ed680f81d5404
Ilan Cosman <ilancosman@gmail.com>
2020-09-08 07:29:24 -0700

    Reorder nvm item

--- functions/_tide_item_nvm.fish
+++ functions/_tide_item_nvm.fish
@@ -2,12 +2,10 @@ function _tide_item_nvm
     if set -l nodePath (command --search node) # Ensure node is installed
         set -l nodeVersion (string split / $nodePath)[-3] # Much faster than node --version
 
-        if test -n "$tide_nvm_default_node"
-            if test "$tide_nvm_default_node" != "$nodeVersion"
-                set_color $tide_nvm_color
-                printf '%s' $tide_nvm_icon' ' $nodeVersion
-            end
-        else if string match --quiet --regex "^$NVM_DIR.*" $nodePath # If node path begins with nvm directory
+        if string match --quiet --regex "^$NVM_DIR.*" $nodePath # If node path begins with nvm directory
+            set_color $tide_nvm_color
+            printf '%s' $tide_nvm_icon' ' $nodeVersion
+        else if test -n "$tide_nvm_default_node" -a "$tide_nvm_default_node" != "$nodeVersion"
             set_color $tide_nvm_color
             printf '%s' $tide_nvm_icon' ' $nodeVersion
         end
