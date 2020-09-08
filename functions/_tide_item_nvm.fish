81cfe19b0f8ddaae9a971e5ca78156bb31530466
Ilan Cosman <ilancosman@gmail.com>
2020-09-07 19:53:12 -0700

    Replace which with command --search

--- functions/_tide_item_nvm.fish
+++ functions/_tide_item_nvm.fish
@@ -1,13 +1,13 @@
 function _tide_item_nvm
-    if set -l whichNode (which node 2>/dev/null) # Ensure node is installed
-        set -l nodeVersion (string split / $whichNode)[-3] # Much faster than node --version
+    if set -l nodePath (command --search node) # Ensure node is installed
+        set -l nodeVersion (string split / $nodePath)[-3] # Much faster than node --version
 
         if test -n "$tide_nvm_default_node"
             if test "$tide_nvm_default_node" != "$nodeVersion"
                 set_color $tide_nvm_color
                 printf '%s' $tide_nvm_icon' ' $nodeVersion
             end
-        else if string match --quiet --regex "^$NVM_DIR.*" $whichNode # If node path begins with nvm directory
+        else if string match --quiet --regex "^$NVM_DIR.*" $nodePath # If node path begins with nvm directory
             set_color $tide_nvm_color
             printf '%s' $tide_nvm_icon' ' $nodeVersion
         end
