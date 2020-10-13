b0982441aa57e034b7a37ee848cd7b9ebc8affad
Ilan Cosman <ilancosman@gmail.com>
2020-10-13 07:41:54 -0700

    Add set_color normal to right_prompt (fixes #33)

--- tide_theme/configure/fish_prompt.fish
+++ tide_theme/configure/fish_prompt.fish
@@ -16,4 +16,7 @@ end
 
 function fish_right_prompt
     printf '%s' $_tide_fish_right_prompt_display
+    # Right prompt is always the last thing on the line 
+    # therefore reset colors for tab completion
+    set_color normal
 end
