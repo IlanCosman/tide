a5d48b180b713e949a8d79dde957443490dc02a7
Ilan Cosman <ilancosman@gmail.com>
2020-10-16 15:45:40 -0700

    Minor edits

--- functions/_tide_item_cmd_duration.fish
+++ functions/_tide_item_cmd_duration.fish
@@ -4,17 +4,16 @@ function _tide_item_cmd_duration
         set -l minutes (math --scale=0 "$CMD_DURATION/60000" % 60)
         set -l hours (math --scale=0 "$CMD_DURATION/3600000" % 60)
 
-        if test $seconds -eq 0
-            set -e seconds
-        end
-        if test $minutes -eq 0
-            set -e minutes
-        end
-        if test $hours -eq 0
-            set -e hours
-        end
-
         set_color $tide_cmd_duration_color
-        printf '%s' $hours'h ' $minutes'm ' $seconds's'
+
+        if test $hours -gt 0
+            printf '%s' $hours'h '
+        end
+        if test $minutes -gt 0
+            printf '%s' $minutes'm '
+        end
+        if test $seconds -gt 0
+            printf '%s' $seconds's'
+        end
     end
-end 
+end
