592767e04d0d7bd44eba8d83c432d5e188eb4acf
Jorge Bucaran <mail@jorgebucaran.com>
2020-11-03 14:39:54 +0900

    Erase _tide_* "private" functions; close #42
    If we don't erase _tide functions, they remain in the global scope. 
    This is problematic for functions that register event handlers as
    they may fire even after Tide is uninstalled, e.g. _tide_quit. #42

--- conf.d/_tide_init.fish
+++ conf.d/_tide_init.fish
@@ -53,6 +53,7 @@ function _tide_init_uninstall --on-event _tide_init_uninstall
     end
     set -e _tide_var_immutable_list
     set -e _tide_var_list
+    functions -e (functions --all | string match -- --entire --regex '^_tide_')
 end
 
 function _set_immutable -a var_name
