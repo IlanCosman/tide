932880bb6dcb827b8ea1878334880a34860bc0da
Jorge Bucaran <mail@jorgebucaran.com>
2020-11-12 01:30:25 +0900

    Remove fish_right_prompt from wrong file

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -13,10 +13,3 @@ function fish_prompt --description 'Created by tide configure'
     _tide_right_prompt
     _tide_left_prompt
 end
-
-function fish_right_prompt
-    printf '%s' $_tide_fish_right_prompt_display
-    # Right prompt is always the last thing on the line 
-    # therefore reset colors for tab completion
-    set_color normal
-end
