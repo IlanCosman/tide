1d4861deb846affc43cfca43004ffcd6d129088c
Ilan Cosman <ilancosman@gmail.com>
2020-11-24 14:24:36 -0800

    Fix status bug

--- functions/_tide_item_status.fish
+++ functions/_tide_item_status.fish
@@ -19,7 +19,7 @@ function _tide_item_status
             printf '%s' $tide_status_failure_icon' '
         end
 
-        __fish_pipestatus_with_signal $_tide_last_pipestatus | string replace 'SIG' '' | string join '|'
+        __fish_pipestatus_with_signal (string split ' ' $_tide_last_pipestatus) | string replace 'SIG' '' | string join '|'
     else if test "$tide_status_always_display" = 'true'
         set -g tide_status_bg_color $tide_status_success_bg_color
         set_color $tide_status_success_color
