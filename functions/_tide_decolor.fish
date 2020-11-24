3f7ed96687869443c66250a045034d0bc9c74f7d
Ilan Cosman <ilancosman@gmail.com>
2020-11-24 12:59:30 -0800

    Fix tests

--- functions/_tide_decolor.fish
+++ functions/_tide_decolor.fish
@@ -1,3 +1,3 @@
-function _tide_decolor -a text
-    string replace --all --regex '\e(\[[\d;]*|\(B\e\[)m' '' "$text"
+function _tide_decolor
+    string replace --all --regex '\e(\[[\d;]*|\(B\e\[)m' '' "$argv"
 end
