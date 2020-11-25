0122bdc62bc241ff0f84971e1b04c3ca030825b3
Ilan Cosman <ilancosman@gmail.com>
2020-11-24 20:20:39 -0800

    Simplify _tide_left_prompt

--- functions/_tide_left_prompt.fish
+++ functions/_tide_left_prompt.fish
@@ -1,84 +1,60 @@
 function _tide_left_prompt
-    set lastItemWasNewline # Display prefix instead of separator before first item
-    set color normal
-    set currentHeight 1
+    set lastItemWasNewline
 
     for item in $tide_left_prompt_items
         if test "$item" = 'newline'
-            _tide_print_frame
-
-            if not set -q lastItemWasNewline && not set -q lastItemWasPromptChar
-                set_color $previousColor -b normal
-                printf '%s' $tide_left_prompt_suffix
-            end
-
-            printf '%b' '\n'
-
+            set_color $previousColor -b normal
+            printf '%s' $tide_left_prompt_suffix\n
             set lastItemWasNewline
-            set currentHeight (math $currentHeight +1)
+            set alreadyOneNewline
+        else
+            set -l output (_tide_item_$item)
+            if test -n "$output"
+                set -l colorName tide_"$item"_bg_color
+                set -l color $$colorName
+                test -n "$color" || set color normal
 
-            continue
-        end
+                if set -e lastItemWasNewline
+                    if test "$tide_left_prompt_frame_enabled" = 'true'
+                        set_color $tide_left_prompt_frame_color -b normal
+                        if set -q alreadyOneNewline
+                            printf '%s' '╰─'
+                        else
+                            printf '%s' '╭─'
+                        end
+                    end
 
-        set -l output (_tide_item_$item)
-
-        if test -n "$output"
-            _tide_print_frame
-
-            set -l colorName tide_"$item"_bg_color
-            set -l color $$colorName
-            test -z "$color" && set color normal
-
-            if set -e lastItemWasNewline || set -e lastItemWasPromptChar
-                if test "$item" != 'prompt_char'
-                    set_color $color -b normal
-                    printf '%s' $tide_left_prompt_prefix
-                end
-            else
-                if test "$color" = "$previousColor"
-                    set_color $tide_left_prompt_item_separator_same_color_color
-                    printf '%s' $tide_left_prompt_item_separator_same_color
+                    if test "$item" != 'prompt_char'
+                        set_color $color -b normal
+                        printf '%s' $tide_left_prompt_prefix
+                    end
                 else
-                    set_color $previousColor -b $color
-                    printf '%s' $tide_left_prompt_item_separator_diff_color
+                    if test "$color" = "$previousColor"
+                        set_color $tide_left_prompt_item_separator_same_color_color
+                        printf '%s' $tide_left_prompt_item_separator_same_color
+                    else
+                        set_color $previousColor -b $color
+                        printf '%s' $tide_left_prompt_item_separator_diff_color
+                    end
                 end
+
+                set_color -b $color
+
+                if test "$tide_left_prompt_pad_items" = 'true' -a "$item" != 'prompt_char'
+                    printf '%s' " $output"(set_color -b $color; or echo)' ' # The set_color is for git_prompt which resets the background color
+                else
+                    printf '%s' "$output"
+                end
+
+                set previousColor $color
+
+                test "$item" = 'prompt_char' && set lastItemWasPromptChar
             end
-
-            set_color -b $color
-
-            if test "$tide_left_prompt_pad_items" = 'true' -a "$item" != 'prompt_char'
-                printf '%s' " $output"(set_color -b $color; or echo)' ' # The set_color is for git_prompt which resets the background color
-            else
-                printf '%s' "$output"
-            end
-
-            set previousColor $color
-
-            test "$item" = 'prompt_char' && set lastItemWasPromptChar
         end
     end
 
-    if set -q lastItemWasNewline
-        if test "$tide_left_prompt_frame_enabled" = 'true'
-            set_color $tide_left_prompt_frame_color
-            printf '%s' '╰─'
-        end
-    else if not set -q lastItemWasPromptChar
-        set_color $previousColor -b normal
-        printf '%s' $tide_left_prompt_suffix
-    end
-end
-
-function _tide_print_frame --no-scope-shadowing
     if set -q lastItemWasNewline && test "$tide_left_prompt_frame_enabled" = 'true'
         set_color $tide_left_prompt_frame_color -b normal
-
-        if test $currentHeight -eq 1
-            printf '%s' '╭─'
-        else if test $currentHeight -lt $_tide_left_prompt_height
-            printf '%s' '├─'
-        else
-            printf '%s' '╰─'
-        end
+        printf '%s' '╰─'
     end
 end
--- functions/_tide_prompt.fish
+++ functions/_tide_prompt.fish
@@ -2,7 +2,7 @@ function _tide_prompt
     test "$tide_print_newline_before_prompt" = 'true' && printf '%b' '\n'
 
     set_color $tide_prompt_connection_color
-    test -z "$tide_prompt_connection_icon" && set -l tide_prompt_connection_icon ' '
+    test -n "$tide_prompt_connection_icon" || set -l tide_prompt_connection_icon ' '
     string repeat --no-newline --max $COLUMNS $tide_prompt_connection_icon
     printf '%b' '\r'
 
