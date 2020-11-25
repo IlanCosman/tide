6015512d8ef5add912c88e2acd9a390b3f027cdb
Ilan Cosman <ilancosman@gmail.com>
2020-11-24 20:22:21 -0800

    Fix right prompt bug

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,5 +1,6 @@
 function fish_right_prompt
-    printf '%s' $_tide_right_prompt_display
+    set -l displayVarName _tide_right_prompt_display_$fish_pid
+    printf '%s' $$displayVarName
     # Right prompt is always the last thing on the line 
     # therefore reset colors for tab completion
     set_color normal
