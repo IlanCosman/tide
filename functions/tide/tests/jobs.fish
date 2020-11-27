fa8fad2faa64af22e9d1345c3eb7647879892687
Ilan Cosman <ilancosman@gmail.com>
2020-11-26 18:19:34 -0800

    Update jobs test

--- functions/tide/tests/jobs.fish
+++ functions/tide/tests/jobs.fish
@@ -1,10 +1,16 @@
 @mesg '-------------jobs-------------'
 
-function _jobs
+function _jobs -a number
+    set -lx _tide_jobs_number $number
     _tide_decolor (_tide_item_jobs)
 end
 
-@test 'No Jobs' -z (_jobs)
+@test 'No Jobs' -z (_jobs 0)
 
 set -lx tide_jobs_icon ''
-@test 'Jobs' (sleep 60 &; _jobs) = ''
+
+set -lx tide_jobs_verbose false
+@test 'Jobs' (_jobs 2) = ''
+
+set -lx tide_jobs_verbose true
+@test 'Jobs' (_jobs 2) = ' 2'
