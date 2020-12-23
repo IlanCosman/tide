c2213d5abcc311df90f00e4b3db5c00cbb32df18
Ilan Cosman <ilancosman@gmail.com>
2020-12-23 11:35:33 -0800

    Remove defunct NVM_DIR variable

--- conf.d/_tide_init.fish
+++ conf.d/_tide_init.fish
@@ -7,7 +7,6 @@ function _tide_init_install --on-event _tide_init_install
 
     _set_immutable _tide_dir "$__fish_config_dir/functions/tide"
 
-    _set_immutable NVM_DIR $HOME/.nvm
     _set_immutable VIRTUAL_ENV_DISABLE_PROMPT true
 
     set -U _tide_var_list
