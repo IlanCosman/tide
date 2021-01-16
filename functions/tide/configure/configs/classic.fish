f3b4e6aa98445cbe3def28b609e51015cc9b42ac
Ilan Cosman <ilancosman@gmail.com>
2021-01-15 20:13:28 -0800

    Remove git from tide_pwd_anchors, left there by mistake

--- functions/tide/configure/configs/classic.fish
+++ functions/tide/configure/configs/classic.fish
@@ -54,7 +54,7 @@ tide_prompt_char_vi_replace_icon '▶'
 tide_prompt_char_vi_visual_icon 'V'
 tide_prompt_connection_color 6C6C6C
 tide_prompt_connection_icon ''
-tide_pwd_anchors 'first' 'last' 'git'
+tide_pwd_anchors 'first' 'last'
 tide_pwd_bg_color 444444
 tide_pwd_color_anchors $_tide_color_light_blue
 tide_pwd_color_dirs $_tide_color_dark_blue
