b6af7e1cef717a22d0c7a07761634d0c10422f4c
Ilan Cosman <ilancosman@gmail.com>
2021-01-27 09:51:11 -0800

    Improve decolor test

--- tests/decolor.fish
+++ tests/decolor.fish
@@ -1,10 +1,9 @@
 # RUN: %fish %s
 
-set -lx decolorText 'Hello Foo'
-
 function _decolor -a color
     _tide_decolor (set_color $color || echo)'Hello Foo'
 end
 
 _decolor red # CHECK: Hello Foo
 _decolor normal # CHECK: Hello Foo
+_decolor $_tide_color_gold # CHECK: Hello Foo
