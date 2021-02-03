ee416126638eba5d3dbc00b9a9072c1063dbc59b
Ilan Cosman <ilancosman@gmail.com>
2021-02-03 13:01:21 -0800

    Add completions for bug-report subcommand

--- completions/tide.fish
+++ completions/tide.fish
@@ -2,3 +2,6 @@ complete -c tide -x -n '__fish_use_subcommand' -a --help -d "Print help message"
 complete -c tide -x -n '__fish_use_subcommand' -a --version -d "Print tide version"
 complete -c tide -x -n '__fish_use_subcommand' -a bug-report -d "Print info for use in bug reports"
 complete -c tide -x -n '__fish_use_subcommand' -a configure -d "Run the configuration wizard"
+
+complete -c tide -x -n '__fish_seen_subcommand_from bug-report' -a --clean -d "Run clean Fish instance and install Tide"
+complete -c tide -x -n '__fish_seen_subcommand_from bug-report' -a --verbose -d "Print full Tide configuration"
