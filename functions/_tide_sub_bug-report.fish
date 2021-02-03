0c5ecf644564d029684af7613a89d23163a9eab2
Ilan Cosman <ilancosman@gmail.com>
2021-02-03 12:57:13 -0800

    Add bug-report --clean

--- functions/_tide_sub_bug-report.fish
+++ functions/_tide_sub_bug-report.fish
@@ -1,14 +1,17 @@
 function _tide_sub_bug-report
-    argparse 'v/verbose' -- $argv
+    argparse 'c/clean' 'v/verbose' -- $argv
 
-    fish --version
-    tide --version
-    printf '%s\n' $TERM
-
-    if set -q _flag_verbose
-        printf '%s\n'
+    if set -q _flag_clean
+        HOME=(mktemp -d) fish --init-command "curl --silent \
+        https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish |
+        source && fisher install ilancosman/tide"
+    else if set -q _flag_verbose
         for var in $_tide_var_list
             printf '%s\n' "$var $$var"
         end
+    else
+        fish --version
+        tide --version
+        printf '%s\n' $TERM
     end
 end
