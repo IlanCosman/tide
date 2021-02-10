8f276758dee1a9ea3e131eb28edfd1fdb6a66845
Ilan Cosman <ilancosman@gmail.com>
2021-02-10 13:59:36 -0800

    Add nvm item test

---
+++ tests/_tide_item_nvm.test.fish
@@ -0,0 +1,16 @@
+# RUN: %fish %s
+
+function _nvm
+    _tide_decolor (_tide_item_nvm)
+end
+
+set -lx tide_nvm_icon ⬢
+
+functions --erase nvm
+_nvm # CHECK:
+
+mock nvm current "echo v15.8.0"
+_nvm # CHECK: ⬢ v15.8.0
+
+mock nvm current "echo system"
+_nvm # CHECK:
