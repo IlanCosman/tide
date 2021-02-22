2620cb349ad4cf4ff88b82a119cdfe2c4ab99dbf
Ilan Cosman <ilancosman@gmail.com>
2021-02-22 14:23:52 -0800

    Support toor and Administrator in context item

--- functions/_tide_item_context.fish
+++ functions/_tide_item_context.fish
@@ -2,7 +2,7 @@ function _tide_item_context
     if set -q SSH_TTY
         set_color $tide_context_ssh_color
         printf '%s' $USER'@'$hostname
-    else if test $USER = 'root'
+    else if contains -- $USER root toor Administrator
         set_color $tide_context_root_color
         printf '%s' $USER'@'$hostname
     else if test "$tide_context_always_display" = 'true'
