39e96090a075e3f53a945df5a33aa58afd159b6c
Ilan Cosman <ilancosman@gmail.com>
2021-02-25 14:20:20 -0800

    Add php item test

---
+++ tests/_tide_item_php.test.fish
@@ -0,0 +1,21 @@
+# RUN: %fish %s
+
+function _php
+    _tide_decolor (_tide_item_php)
+end
+
+set -l phpDir ~/phpTest
+mkdir -p $phpDir
+cd $phpDir
+
+mock php --version "echo \
+'PHP 8.0.2 (cli) (built: Feb  2 2021 18:26:02) ( NTS )
+Copyright (c) The PHP Group
+Zend Engine v4.0.2, Copyright (c) Zend Technologies'"
+
+_php # CHECK:
+
+touch composer.json
+_php # CHECK:  8.0.2
+
+rm -r $phpDir
