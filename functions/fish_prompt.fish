827625a9694a7be2e2ee2dcd20b98393ac7e612b
Christian Volkmann <christian.volkmann@rwth-aachen.de>
2021-02-26 14:11:34 +0100

    fix: Define empty fish_prompt when non-interactive

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -5,13 +5,16 @@ set -g _tide_left_prompt_display_var _tide_left_prompt_display_$fish_pid
 
 set -gx _tide_fish_pid $fish_pid
 
-function fish_prompt
-    set -lx _tide_last_pipestatus $pipestatus
 
-    if not set -e _tide_repainting
-        set -lx _tide_jobs_number (jobs --pid | count)
 
-        fish --command "
+if status is-interactive
+    function fish_prompt
+        set -lx _tide_last_pipestatus $pipestatus
+
+        if not set -e _tide_repainting
+            set -lx _tide_jobs_number (jobs --pid | count)
+
+            fish --command "
         set CMD_DURATION $CMD_DURATION
         set COLUMNS $COLUMNS
         set fish_bind_mode $fish_bind_mode
@@ -21,26 +24,25 @@ function fish_prompt
         set -U _tide_left_prompt_display_$fish_pid (_tide_prompt)
         " >&- & # >&- closes stdout. See https://github.com/fish-shell/fish-shell/issues/7559
 
-        set -g _tide_last_pid (jobs --last --pid)
-        builtin disown $_tide_last_pid
+            set -g _tide_last_pid (jobs --last --pid)
+            builtin disown $_tide_last_pid
+        end
+
+        string unescape $$_tide_left_prompt_display_var
     end
 
-    string unescape $$_tide_left_prompt_display_var
-end
-
-if status is-interactive
     function _tide_refresh_prompt --on-variable _tide_left_prompt_display_$fish_pid --on-variable _tide_right_prompt_display_$fish_pid
         set -g _tide_repainting
         commandline --function force-repaint
     end
+
+    # Double underscores to avoid erasing this function on uninstall
+    function __tide_on_fish_exit --on-event fish_exit
+        set -e _tide_left_prompt_display_$fish_pid
+        set -e _tide_right_prompt_display_$fish_pid
+    end
 else
-    function _tide_refresh_prompt --on-variable _tide_left_prompt_display_$fish_pid --on-variable _tide_right_prompt_display_$fish_pid
+    function fish_prompt
         # do nothing when not interactive
     end
-end
-
-# Double underscores to avoid erasing this function on uninstall
-function __tide_on_fish_exit --on-event fish_exit
-    set -e _tide_left_prompt_display_$fish_pid
-    set -e _tide_right_prompt_display_$fish_pid
 end
