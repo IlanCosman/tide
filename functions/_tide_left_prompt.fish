f48828136a181e52ba7918f93f81560460b446d4
Ilan Cosman <ilancosman@gmail.com>
2021-03-04 11:06:17 -0800

    More refactoring of print logic

--- functions/_tide_left_prompt.fish
+++ functions/_tide_left_prompt.fish
@@ -1,46 +1,44 @@
 function _tide_left_prompt
-    set lastItemWasNewline
+    set -l lastItem newline
 
     for item in $tide_left_prompt_items
         if test "$item" = newline
             set_color $previousColor -b normal
             printf '%s' $tide_left_prompt_suffix\n
-            set lastItemWasNewline
         else
             set -l output (_tide_item_$item)
-            if test -n "$output"
-                set -l colorName tide_"$item"_bg_color
-                set -l color $$colorName
+            test -n "$output" || continue
 
-                if set -e lastItemWasNewline
-                    if test "$item" != prompt_char
-                        set_color $color -b normal
-                        printf '%s' $tide_left_prompt_prefix
-                    end
-                else if test "$color" = "$previousColor"
-                    set_color $tide_left_prompt_item_separator_same_color_color
-                    printf '%s' $tide_left_prompt_item_separator_same_color
-                else
-                    set_color $previousColor -b $color
-                    printf '%s' $tide_left_prompt_item_separator_diff_color
+            set -l colorName tide_"$item"_bg_color
+            set -l color $$colorName
+
+            if test "$lastItem" = newline
+                if test "$item" != prompt_char
+                    set_color $color -b normal
+                    printf '%s' $tide_left_prompt_prefix
                 end
-
-                set_color -b $color
-
-                if test "$tide_left_prompt_pad_items" = true -a "$item" != prompt_char
-                    printf '%s' " $output "
-                else
-                    printf '%s' "$output"
-                end
-
-                set previousColor $color
-
-                test "$item" = prompt_char && set lastItemWasPromptChar
+            else if test "$color" = "$previousColor"
+                set_color $tide_left_prompt_item_separator_same_color_color
+                printf '%s' $tide_left_prompt_item_separator_same_color
+            else
+                set_color $previousColor -b $color
+                printf '%s' $tide_left_prompt_item_separator_diff_color
             end
+
+            set_color -b $color
+
+            if test "$tide_left_prompt_pad_items" = true -a "$item" != prompt_char
+                printf '%s' " $output "
+            else
+                printf '%s' "$output"
+            end
+
+            set previousColor $color
         end
+        set lastItem $item
     end
 
-    if not set -q lastItemWasNewline && not set -q lastItemWasPromptChar
+    if test "$lastItem" != newline -a "$lastItem" != prompt_char
         set_color $previousColor -b normal
         printf '%s' $tide_left_prompt_suffix
     end
--- functions/_tide_right_prompt.fish
+++ functions/_tide_right_prompt.fish
@@ -1,42 +1,42 @@
 function _tide_right_prompt
-    set lastItemWasNewline
+    set -l lastItem newline
 
     for item in $tide_right_prompt_items
         if test "$item" = newline
             set_color $previousColor -b normal
             printf '%s' $tide_right_prompt_suffix\n
-            set lastItemWasNewline
         else
             set -l output (_tide_item_$item)
-            if test -n "$output"
-                set -l colorName tide_"$item"_bg_color
-                set -l color $$colorName
+            test -n "$output" || continue
 
-                if set -e lastItemWasNewline
-                    set_color $color -b normal
-                    printf '%s' $tide_right_prompt_prefix
-                else if test "$color" = "$previousColor"
-                    set_color $tide_right_prompt_item_separator_same_color_color
-                    printf '%s' $tide_right_prompt_item_separator_same_color
-                else
-                    set_color $color -b $previousColor
-                    printf '%s' $tide_right_prompt_item_separator_diff_color
-                end
+            set -l colorName tide_"$item"_bg_color
+            set -l color $$colorName
 
-                set_color -b $color
-
-                if test "$tide_right_prompt_pad_items" = true
-                    printf '%s' " $output "
-                else
-                    printf '%s' "$output"
-                end
-
-                set previousColor $color
+            if test "$lastItem" = newline
+                set_color $color -b normal
+                printf '%s' $tide_right_prompt_prefix
+            else if test "$color" = "$previousColor"
+                set_color $tide_right_prompt_item_separator_same_color_color
+                printf '%s' $tide_right_prompt_item_separator_same_color
+            else
+                set_color $color -b $previousColor
+                printf '%s' $tide_right_prompt_item_separator_diff_color
             end
+
+            set_color -b $color
+
+            if test "$tide_right_prompt_pad_items" = true
+                printf '%s' " $output "
+            else
+                printf '%s' "$output"
+            end
+
+            set previousColor $color
         end
+        set lastItem $item
     end
 
-    if not set -q lastItemWasNewline
+    if test "$lastItem" != newline
         set_color $previousColor -b normal
         printf '%s' $tide_right_prompt_suffix
     end
