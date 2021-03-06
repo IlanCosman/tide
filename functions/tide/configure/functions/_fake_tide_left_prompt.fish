a8b08571d80b631ecf8798b67dd98b965457c63d
Ilan Cosman <ilancosman@gmail.com>
2021-03-05 17:43:08 -0800

    Update fake prompt for new printing method

--- functions/tide/configure/functions/_fake_tide_left_prompt.fish
+++ functions/tide/configure/functions/_fake_tide_left_prompt.fish
@@ -1,63 +1,47 @@
 function _fake_tide_left_prompt
-    set lastItemWasNewline
+    set -l lastItem newline
 
     for item in $fake_tide_left_prompt_items
         if test "$item" = newline
             set_color $previousColor -b normal
             printf '%s' $fake_tide_left_prompt_suffix\n
-            set lastItemWasNewline
-            set alreadyOneNewline
         else
             set -l output (_fake_tide_item_$item)
-            if test -n "$output"
-                set -l colorName fake_tide_"$item"_bg_color
-                set -l color $$colorName
-                test -n "$color" || set color normal
+            test -n "$output" || continue
 
-                if set -e lastItemWasNewline
-                    if test "$fake_tide_left_prompt_frame_enabled" = true
-                        set_color $fake_tide_left_prompt_frame_color -b normal
-                        if set -q alreadyOneNewline
-                            printf '%s' '╰─'
-                        else
-                            printf '%s' '╭─'
-                        end
-                    end
+            set -l colorName fake_tide_"$item"_bg_color
+            set -l color $$colorName
 
-                    if test "$item" != prompt_char
-                        set_color $color -b normal
-                        printf '%s' $fake_tide_left_prompt_prefix
-                    end
-                else
-                    if test "$color" = "$previousColor"
-                        set_color $fake_tide_left_prompt_item_separator_same_color_color
-                        printf '%s' $fake_tide_left_prompt_item_separator_same_color
-                    else
-                        set_color $previousColor -b $color
-                        printf '%s' $fake_tide_left_prompt_item_separator_diff_color
-                    end
+            if test "$lastItem" = newline
+                if test "$item" != prompt_char
+                    set_color $color -b normal
+                    printf '%s' $fake_tide_left_prompt_prefix
                 end
-
-                set_color -b $color
-
-                if test "$fake_tide_left_prompt_pad_items" = true -a "$item" != prompt_char
-                    printf '%s' " $output "
-                else
-                    printf '%s' "$output"
-                end
-
-                set previousColor $color
-
-                test "$item" = prompt_char && set lastItemWasPromptChar
+            else if test "$color" = "$previousColor"
+                set_color $fake_tide_left_prompt_item_separator_same_color_color
+                printf '%s' $fake_tide_left_prompt_item_separator_same_color
+            else
+                set_color $previousColor -b $color
+                printf '%s' $fake_tide_left_prompt_item_separator_diff_color
             end
+
+            set_color -b $color
+
+            if test "$fake_tide_left_prompt_pad_items" = true -a "$item" != prompt_char
+                printf '%s' " $output "
+            else
+                printf '%s' "$output"
+            end
+
+            set previousColor $color
         end
+        set lastItem $item
     end
 
-    if set -q lastItemWasNewline && test "$fake_tide_left_prompt_frame_enabled" = true
-        set_color $fake_tide_left_prompt_frame_color -b normal
-        printf '%s' '╰─'
-    else if not set -q lastItemWasPromptChar
+    if test "$lastItem" != newline -a "$lastItem" != prompt_char
         set_color $previousColor -b normal
         printf '%s' $fake_tide_left_prompt_suffix
     end
+
+    set_color normal
 end
