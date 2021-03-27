cdfad003f01a5d8fde04fd3d064b3db4973eb977
Ilan Cosman <ilancosman@gmail.com>
2021-03-27 12:58:51 -0700

    Minor edits

--- conf.d/_tide_interactive_init.fish
+++ conf.d/_tide_interactive_init.fish
@@ -9,17 +9,17 @@ set -gx _tide_right_prompt_display_var _tide_right_prompt_display_$fish_pid
 
 function _tide_background_job --on-event fish_prompt --on-variable fish_bind_mode
     _tide_last_status=$status _tide_last_pipestatus=$pipestatus _tide_jobs_number=(jobs --pid | count) fish --command "
-        set CMD_DURATION $CMD_DURATION
-        set COLUMNS $COLUMNS
-        set fish_bind_mode $fish_bind_mode
-        set fish_term24bit $fish_term24bit
+        set -g CMD_DURATION $CMD_DURATION
+        set -g COLUMNS $COLUMNS
+        set -g fish_bind_mode $fish_bind_mode
+        set -g fish_term24bit $fish_term24bit
         set -U $_tide_left_prompt_display_var (_tide_prompt)" </dev/null &
     # Remove </dev/null in Fish 3.3.0, see https://github.com/fish-shell/fish-shell/issues/7842
 
     builtin disown
 
     command kill $_tide_last_pid 2>/dev/null
-    set -g _tide_last_pid (jobs --last --pid)
+    set -g _tide_last_pid (jobs --last --pid) # Replace with $last_pid in Fish 3.3.0
 end
 
 function _tide_refresh_prompt --on-variable $_tide_left_prompt_display_var --on-variable $_tide_right_prompt_display_var
