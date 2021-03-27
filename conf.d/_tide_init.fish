c41010629719dd58339653dd4517185d06ffef7a
Ilan Cosman <ilancosman@gmail.com>
2021-03-27 09:09:56 -0700

    Minor edits to install and uninstall

--- conf.d/_tide_init.fish
+++ conf.d/_tide_init.fish
@@ -12,10 +12,7 @@ function _tide_init_install --on-event _tide_init_install
     _tide_set _tide_color_gold D7AF00
     _tide_set _tide_color_green 5FD700
     _tide_set _tide_color_light_blue 00AFFF
-
-    # string replace is roughly dirname
-    _tide_set _tide_root (status dirname | string replace --regex '/[^/]+$' '')
-
+    _tide_set _tide_root (status dirname)/..
     _tide_set VIRTUAL_ENV_DISABLE_PROMPT true
 
     source $_tide_root/functions/tide/configure/choices/all/style.fish
@@ -35,6 +32,5 @@ end
 
 function _tide_init_uninstall --on-event _tide_init_uninstall
     set -e $_tide_var_list _tide_var_list
-
     functions --erase (functions --all | string match --entire --regex '^_tide_')
 end
