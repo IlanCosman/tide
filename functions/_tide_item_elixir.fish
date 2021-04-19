c29a1a84239fecbea19aca98df98d96f2f99a02d
Gabriel Bellon <gabriel1490@gmail.com>
2021-04-19 19:50:03 +0100

    Remove `sed` -r flag to support OS X

--- functions/_tide_item_elixir.fish
+++ functions/_tide_item_elixir.fish
@@ -1,6 +1,6 @@
 function _tide_item_elixir
     if test -e mix.exs
-        set -l elixirVersion (elixir -v | sed -nr 's/Elixir //;s/compiled with Erlang\///p')
+        set -l elixirVersion (elixir -v | sed -n 's/Elixir //;s/compiled with Erlang\///p')
 
         set_color $tide_elixir_color
         printf '%s' $tide_elixir_icon ' ' $elixirVersion
