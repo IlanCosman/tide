3b0e52bda04f0155f1a1e8ff87750741a5041f3a
Gabriel Bellon <gabriel1490@gmail.com>
2021-04-26 11:48:19 +0100

    Add elixir item to unusable items

--- functions/_tide_remove_unusable_items.fish
+++ functions/_tide_remove_unusable_items.fish
@@ -1,6 +1,6 @@
 function _tide_remove_unusable_items
     # Remove tool-specific items for tools the machine doesn't have installed
-    for item in chruby git nvm php rust virtual_env
+    for item in chruby git nvm php rust virtual_env elixir
         set -l cliName $item
         switch $item
             case virtual_env
