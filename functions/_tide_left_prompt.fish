7756f1ae16b4b831638109d176dc9f126cfa70af
Ilan Cosman <ilancosman@gmail.com>
2021-04-26 13:47:15 -0700

    minor edits

--- functions/_tide_left_prompt.fish
+++ functions/_tide_left_prompt.fish
@@ -3,12 +3,13 @@ function _tide_left_prompt
     set -g _tide_which_side_working_on left
 
     for item in $tide_left_prompt_items
-        if test "$item" = newline
-            set_color $previousColor -b normal
-            printf '%s' $tide_left_prompt_suffix\n
-            set -g tide_last_item newline
-        else
-            _tide_item_$item
+        switch $item
+            case newline
+                set_color $tide_previous_bg_color -b normal
+                printf '%s' $tide_left_prompt_suffix\n
+                set -g tide_last_item newline
+            case '*'
+                _tide_item_$item
         end
     end
 
--- functions/_tide_right_prompt.fish
+++ functions/_tide_right_prompt.fish
@@ -3,12 +3,13 @@ function _tide_right_prompt
     set -g _tide_which_side_working_on right
 
     for item in $tide_right_prompt_items
-        if test "$item" = newline
-            set_color $previousColor -b normal
-            printf '%s' $tide_right_prompt_suffix\n
-            set -g tide_last_item newline
-        else
-            _tide_item_$item
+        switch $item
+            case newline
+                set_color $tide_previous_bg_color -b normal
+                printf '%s' $tide_right_prompt_suffix\n
+                set -g tide_last_item newline
+            case '*'
+                _tide_item_$item
         end
     end
 
