a85c577c819de9cb01ff0d30d1796e03198e24b9
Ilan Cosman <ilancosman@gmail.com>
2021-06-01 17:11:03 -0700

    Remove non-existant tide_time_icon

--- functions/_tide_item_time.fish
+++ functions/_tide_item_time.fish
@@ -1,3 +1,3 @@
 function _tide_item_time
-    _tide_print_item time $tide_time_icon' ' (date +$tide_time_format)
+    _tide_print_item time (date +$tide_time_format)
 end
--- functions/tide/configure/functions/_fake_tide_item_time.fish
+++ functions/tide/configure/functions/_fake_tide_item_time.fish
@@ -1,3 +1,3 @@
 function _fake_tide_item_time
-    _fake_tide_print_item time $tide_time_icon' ' (date +$fake_tide_time_format)
+    _fake_tide_print_item time (date +$fake_tide_time_format)
 end
