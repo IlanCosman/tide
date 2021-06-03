24b981e97c4a7604d8e3fc937fa252111ce5b7c7
Ilan Cosman <ilancosman@gmail.com>
2021-06-03 13:12:38 -0700

    Also check for python3 before removing item (fixes #162)

--- functions/_tide_remove_unusable_items.fish
+++ functions/_tide_remove_unusable_items.fish
@@ -1,13 +1,13 @@
 function _tide_remove_unusable_items
     # Remove tool-specific items for tools the machine doesn't have installed
     for item in chruby git nvm php rustc virtual_env
-        set -l cli_name $item
+        set -l cli_names $item
         switch $item
             case virtual_env
-                set cli_name python
+                set cli_names python python3
         end
 
-        if not type -q $cli_name
+        if not type -q $cli_names
             _tide_find_and_remove $item tide_left_prompt_items
             _tide_find_and_remove $item tide_right_prompt_items
         end
