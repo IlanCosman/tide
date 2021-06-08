6e74d1c71dd8df542ff30d7e6954c0877c834f7e
Ilan Cosman <ilancosman@gmail.com>
2021-06-08 13:21:49 -0700

    Remove nonexistent variable tide_jobs_verbose

--- functions/_tide_item_jobs.fish
+++ functions/_tide_item_jobs.fish
@@ -1,7 +1,3 @@
 function _tide_item_jobs
-    test $_tide_jobs_number != 0 && if test "$tide_jobs_verbose" = true
-        _tide_print_item jobs $tide_jobs_icon' ' $_tide_jobs_number
-    else
-        _tide_print_item jobs $tide_jobs_icon
-    end
+    test -n "$_tide_jobs" && _tide_print_item jobs $tide_jobs_icon
 end
--- tests/_tide_item_jobs.test.fish
+++ tests/_tide_item_jobs.test.fish
@@ -1,16 +1,11 @@
 # RUN: %fish %s
 
-function _jobs -a number
-    set -lx _tide_jobs_number $number
+function _jobs -a jobs
+    set -lx _tide_jobs $jobs
     _tide_decolor (_tide_item_jobs)
 end
 
-_jobs 0 # CHECK:
+_jobs # CHECK:
 
 set -lx tide_jobs_icon ''
-
-set -lx tide_jobs_verbose false
-_jobs 2 # CHECK: 
-
-set -lx tide_jobs_verbose true
-_jobs 2 # CHECK:  2
+_jobs blah # CHECK: 
