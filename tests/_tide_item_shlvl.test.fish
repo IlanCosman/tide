70d4801b754c4ae5db75f2836155a9063f2cf834
Kid <44045911+kidonng@users.noreply.github.com>
2021-06-20 16:37:30 +0800

    Fix macOS test

--- tests/_tide_item_shlvl.test.fish
+++ tests/_tide_item_shlvl.test.fish
@@ -4,8 +4,16 @@ function _shlvl
     _tide_decolor (fish -c _tide_item_shlvl)
 end
 
-_shlvl # CHECK:  2
+if test (uname) = Darwin
+    _shlvl # CHECK:  4
 
-set -lx tide_shlvl_threshold 2
+    set -lx tide_shlvl_threshold 4
 
-_shlvl # CHECK:
+    _shlvl # CHECK:
+else
+    _shlvl # CHECK:  2
+
+    set -lx tide_shlvl_threshold 2
+
+    _shlvl # CHECK:
+end
