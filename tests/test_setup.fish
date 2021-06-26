0172b891603d20afaaddcbd09de5d396b7ed6321
Ilan Cosman <ilancosman@gmail.com>
2021-06-26 14:23:08 -0700

    set TERM for tests

--- tests/test_setup.fish
+++ tests/test_setup.fish
@@ -1,5 +1,6 @@
 function _tide_decolor
     string replace --all --regex '\e(\[[\d;]*|\(B\e\[)m(\co)?' '' "$argv"
 end
-
 funcsave _tide_decolor
+
+echo "set TERM xterm-256color" >$__fish_config_dir/config.fish
