159e99fec8f7fa411a368614bfb873a9d459a6aa
Ilan Cosman <ilancosman@gmail.com>
2021-06-29 14:41:52 -0700

    Update SHLVL item for fish 3.3

--- functions/_tide_item_shlvl.fish
+++ functions/_tide_item_shlvl.fish
@@ -1,5 +1,4 @@
 function _tide_item_shlvl
-    # Item functions run in a deeper level of shell
-    set -l real_shlvl (math $SHLVL - 1)
-    test $real_shlvl -gt $tide_shlvl_threshold && _tide_print_item shlvl $tide_shlvl_icon' ' $real_shlvl
+    # Non-interactive shells do not increment SHLVL, so we don't need to subtract 1
+    test $SHLVL -gt $tide_shlvl_threshold && _tide_print_item shlvl $tide_shlvl_icon' ' $SHLVL
 end
