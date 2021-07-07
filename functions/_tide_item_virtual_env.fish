7c5d5975eb1c07dd9665d156d4644297f220980d
William Brown <Will@WillJBrown.com>
2021-07-07 23:55:13 +0100

    Feat: Handle Pipenv Virtual Environment Names This gets current directory name and compares with pipenv's naming pattern. If it matches the output os changed to the directory name instead or the virtual environment name.

--- functions/_tide_item_virtual_env.fish
+++ functions/_tide_item_virtual_env.fish
@@ -1,9 +1,13 @@
 function _tide_item_virtual_env
     if set -l splitVirtualEnv (string split '/' "$VIRTUAL_ENV")
         set_color $tide_virtual_env_color
+        set -l directoryName (string split '/' (pwd))[-1]
+        string match -qe $directoryName- $splitVirtualEnv[-1]; set pipenvStyle $status
         # Avoid printing a generic name
         if contains -- $splitVirtualEnv[-1] virtualenv venv .venv env
             printf '%s' $tide_virtual_env_icon' ' $splitVirtualEnv[-2]
+        else if test $pipenvStyle -eq 0
+            printf '%s' $tide_virtual_env_icon' ' $directoryName
         else
             printf '%s' $tide_virtual_env_icon' ' $splitVirtualEnv[-1]
         end
