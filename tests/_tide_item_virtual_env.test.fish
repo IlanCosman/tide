0c5ba87d561af61dd4928a3b25999168fcf4b572
Ilan Cosman <ilancosman@gmail.com>
2021-07-08 11:27:43 -0700

    Support pipenv in virtual_env item (#170)
    Co-authored-by: William Brown <Will@WillJBrown.com>

--- tests/_tide_item_virtual_env.test.fish
+++ tests/_tide_item_virtual_env.test.fish
@@ -9,8 +9,11 @@ set -lx tide_virtual_env_icon ''
 set -lx VIRTUAL_ENV
 _virtual_env # CHECK:
 
-set -lx VIRTUAL_ENV ~/python_project/non-generic-name
+set -lx VIRTUAL_ENV /home/ilan/python_project/non-generic-name
 _virtual_env # CHECK:  non-generic-name
 
-set -lx VIRTUAL_ENV ~/python_project/venv
+set -lx VIRTUAL_ENV /home/ilan/python_project/venv
 _virtual_env # CHECK:  python_project
+
+set -lx VIRTUAL_ENV /home/ilan/.local/share/virtualenvs/pipenv_project-EwRYuc3l
+_virtual_env # CHECK:  pipenv_project
