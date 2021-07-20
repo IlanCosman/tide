1d8c965bb71410e0b0022c4ce241ba86f7c5df27
Ilan Cosman <ilancosman@gmail.com>
2021-07-19 20:56:12 -0700

    Minor edits

--- functions/_tide_item_kubectl.fish
+++ functions/_tide_item_kubectl.fish
@@ -1,4 +1,4 @@
 function _tide_item_kubectl
-    set -l context (kubectl config view --minify --output 'jsonpath={.current-context}/{..namespace}' 2>/dev/null)
-    test -n "$context" && _tide_print_item kubectl $tide_kubectl_icon' ' (string replace -r '/(?:default)?$' '' $context)
+    set -l context (kubectl config view --minify --output 'jsonpath={.current-context}/{..namespace}' 2>/dev/null) &&
+        _tide_print_item kubectl $tide_kubectl_icon' ' (string replace --regex '/(|default)$' '' $context)
 end
