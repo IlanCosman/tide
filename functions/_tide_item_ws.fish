78c4049acdb38f160c0b8ff738b6b490a1e294f8
Dave Williams <dave@dave.io>
2021-08-30 18:28:14 +0100

    oops, use ws colours not node in _tide_item_ws

--- functions/_tide_item_ws.fish
+++ functions/_tide_item_ws.fish
@@ -1,3 +1,3 @@
 function _tide_item_ws
-    test -n "$WS_ENVIRONMENT" && _tide_print_item node $tide_ws_icon' ' (ws show)
+    test -n "$WS_ENVIRONMENT" && _tide_print_item ws $tide_ws_icon' ' (ws show)
 end
