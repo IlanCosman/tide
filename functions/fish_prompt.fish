6b26c69da8d8becf7ff041fb14927768d0efeb67
Ilan Cosman <ilancosman@gmail.com>
2021-09-26 18:14:26 -0700

    Minor edits

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -9,6 +9,7 @@ _tide_cache_variables
 # The first element in $$_tide_prompt_var is right prompt
 # All remaining ones are 'left' prompt (also upper right in 2-line prompts)
 set -g _tide_prompt_var _tide_prompt_$fish_pid
+set -U $_tide_prompt_var # So that if we erase $_tide_prompt_var, the bg job can't set a uvar
 
 function _tide_refresh_prompt --on-variable $_tide_prompt_var
     set -g _tide_self_repainting # prevents us from creating a second background job
@@ -19,7 +20,7 @@ function fish_prompt
     _tide_last_status=$status _tide_last_pipestatus=$pipestatus if not set -e _tide_self_repainting
         jobs --query
         fish --command "_tide_jobs_status=$status CMD_DURATION=$CMD_DURATION COLUMNS=$COLUMNS \
-            fish_bind_mode=$fish_bind_mode set -U $_tide_prompt_var (_tide_prompt)" &
+            fish_bind_mode=$fish_bind_mode set $_tide_prompt_var (_tide_prompt)" &
         builtin disown
 
         command kill $_tide_last_pid 2>/dev/null
