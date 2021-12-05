886b94a6d8a2552d99b751bd50c2dd300504f614
Ilan Cosman <ilancosman@gmail.com>
2021-12-04 19:25:40 -0800

    Finish pipeline in _tide_item_status

--- functions/_tide_item_status.fish
+++ functions/_tide_item_status.fish
@@ -3,13 +3,11 @@ function _tide_item_status
         if test "$_tide_pipestatus" = 1 # If simple failure
             contains character $_tide_left_items || tide_status_bg_color=$tide_status_bg_color_failure \
                 tide_status_color=$tide_status_color_failure _tide_print_item status $tide_status_icon_failure' ' 1
-        else if test $_tide_status = 0
-            _tide_print_item status $tide_status_icon' ' \
-                (fish_status_to_signal $_tide_pipestatus | string replace SIG '' | string join '|')
         else
-            tide_status_bg_color=$tide_status_bg_color_failure tide_status_color=$tide_status_color_failure \
-                _tide_print_item status $tide_status_icon_failure' ' \
-                (fish_status_to_signal $_tide_pipestatus | string replace SIG '' | string join '|')
+            fish_status_to_signal $_tide_pipestatus | string replace SIG '' | string join '|' | read -l out
+            test $_tide_status = 0 && _tide_print_item status $tide_status_icon' ' $out ||
+                tide_status_bg_color=$tide_status_bg_color_failure tide_status_color=$tide_status_color_failure \
+                    _tide_print_item status $tide_status_icon_failure' ' $out
         end
     else if not contains character $_tide_left_items
         _tide_print_item status $tide_status_icon
