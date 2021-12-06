4c871ee9189d638fac1683d55797ad0da57d14bf
Gabriel Augendre <gabriel@augendre.info>
2021-12-06 18:07:03 +0100

    Use docker context inspect instead of show
    The latter is not available on all systems.

--- functions/_tide_item_docker.fish
+++ functions/_tide_item_docker.fish
@@ -1,5 +1,5 @@
 function _tide_item_docker
-    set -l context (_tide_item_docker_replace_ignore_result (docker context show 2>/dev/null)) &&
+    set -l context (_tide_item_docker_replace_ignore_result (docker context inspect --format "{{.Name}}" 2>/dev/null)) &&
         test -n "$context" &&
         _tide_print_item docker $tide_docker_icon' ' $context
 end
