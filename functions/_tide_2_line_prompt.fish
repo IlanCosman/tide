3ef3929d20821563c5f84f30a00f76240bc89f4e
Ilan Cosman <ilancosman@gmail.com>
2021-12-08 11:08:33 -0800

    Remove useless code

--- functions/_tide_2_line_prompt.fish
+++ functions/_tide_2_line_prompt.fish
@@ -2,19 +2,11 @@ function _tide_2_line_prompt
     set -g add_prefix
     _tide_side=left set -f left (for item in $_tide_left_items
             _tide_item_$item
-        end
-        if not set -e add_prefix
-            set_color $prev_bg_color -b normal
-            echo -ns $tide_left_prompt_suffix
         end)
 
     set -g add_prefix
     _tide_side=right set -f right (for item in $_tide_right_items
             _tide_item_$item
-        end
-        if not set -e add_prefix
-            set_color $prev_bg_color -b normal
-            echo -ns $tide_right_prompt_suffix
         end)
 
     test "$tide_left_prompt_frame_enabled" = true &&
