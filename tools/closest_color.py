3ab83dfcc64357fe4820630705074eb4b2e8a549
Ilan Cosman <ilancosman@gmail.com>
2021-12-07 22:09:49 -0800

    Make scripts executable

--- tools/closest_color.py
+++ tools/closest_color.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 
 from colormath.color_conversions import convert_color
--- tools/swatch.fish
+++ tools/swatch.fish
@@ -1,3 +1,5 @@
+#!/usr/bin/env fish
+
 set -l height_in_lines 2
 
 set -l width_in_chars (math --scale=0 $height_in_lines x 2.5) # 2.5 = ratio
