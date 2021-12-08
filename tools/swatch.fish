4c1931dffd3c53a07cb4f42f79c87f6a37d026df
Ilan Cosman <ilancosman@gmail.com>
2021-12-07 22:01:39 -0800

    Add color swatch script

---
+++ tools/swatch.fish
@@ -0,0 +1,12 @@
+set -l height_in_lines 2
+
+set -l width_in_chars (math --scale=0 $height_in_lines x 2.5) # 2.5 = ratio
+
+for arg in $argv
+    for i in (seq $height_in_lines)
+        set_color -b $arg
+        string repeat --no-newline --count $width_in_chars ' '
+        set_color normal
+        echo
+    end
+end
