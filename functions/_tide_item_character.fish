1f0a972ac00d938b3307a2fdb6da1e37f461a95b
Ilan Cosman <ilancosman@gmail.com>
2021-12-12 02:17:44 -0800

    Fix character item spacing issue

--- functions/_tide_item_character.fish
+++ functions/_tide_item_character.fish
@@ -1,6 +1,8 @@
 function _tide_item_character
     test $_tide_status = 0 && set_color $tide_character_color || set_color $tide_character_color_failure
 
+    set -q add_prefix || echo -ns ' '
+
     test "$fish_key_bindings" = fish_default_key_bindings && echo -ns $tide_character_icon ||
         switch $fish_bind_mode
             case insert
--- functions/tide/configure/functions/_fake_tide_item_character.fish
+++ functions/tide/configure/functions/_fake_tide_item_character.fish
@@ -1,4 +1,5 @@
 function _fake_tide_item_character
     set_color $fake_tide_character_color
+    contains newline $fake_tide_left_prompt_items || echo -ns ' '
     echo -ns $fake_tide_character_icon
 end
