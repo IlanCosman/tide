08ede7ed36e42f34bf6c63be3c6c5f60b55e946f
Ilan Cosman <ilancosman@gmail.com>
2021-12-12 12:27:53 -0800

    Horizontally display swatches

--- tools/swatch.fish
+++ tools/swatch.fish
@@ -4,11 +4,11 @@ set -l height_in_lines 2
 
 set -l width_in_chars (math --scale=0 $height_in_lines x 2.5) # 2.5 = ratio
 
-for arg in $argv
-    for i in (seq $height_in_lines)
+for i in (seq $height_in_lines)
+    for arg in $argv
         set_color -b $arg
         string repeat --no-newline --count $width_in_chars ' '
         set_color normal
-        echo
     end
+    echo
 end
