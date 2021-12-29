3a60a36cc9f5244c4754e5c74366bea9893e59e7
Ilan Cosman <ilancosman@gmail.com>
2021-12-28 16:05:27 -0800

    Work

---
+++ functions/tide/configure/configs/all/all.fish
@@ -0,0 +1,45 @@
+tide_character_color $_light_green
+tide_character_color_failure $_light_red
+tide_character_icon ❯
+tide_character_vi_icon_default ❮
+tide_character_vi_icon_replace ▶
+tide_character_vi_icon_visual V
+tide_chruby_icon 
+tide_cmd_duration_decimals 0
+tide_cmd_duration_icon
+tide_cmd_duration_threshold 3000
+tide_context_always_display false
+tide_context_color_default $_ssh
+tide_context_color_root $_gold
+tide_context_color_ssh $_ssh
+tide_git_icon
+tide_git_truncation_length 24
+tide_go_icon 
+tide_jobs_icon 
+tide_kubectl_icon ⎈
+tide_left_prompt_prefix ''
+tide_node_icon ⬢
+tide_os_icon (_tide_detect_os)
+tide_php_icon 
+tide_private_mode_icon 﫸
+tide_prompt_add_newline_before true
+tide_prompt_color_frame_and_connection $_gray
+tide_prompt_color_separator_same_color $_light_gray
+tide_prompt_icon_connection ' '
+tide_prompt_min_cols 26
+tide_pwd_icon
+tide_pwd_icon_home
+tide_pwd_icon_unwritable 
+tide_pwd_markers .bzr .citc .git .hg .node-version .python-version .ruby-version .shorten_folder_marker .svn .terraform Cargo.toml composer.json CVS go.mod package.json
+tide_right_prompt_items status cmd_duration context jobs node virtual_env rustc php chruby go kubectl toolbox terraform
+tide_right_prompt_suffix ''
+tide_rustc_icon 
+tide_shlvl_icon 
+tide_shlvl_threshold 1
+tide_status_color $_green
+tide_status_icon ✔
+tide_status_icon_failure ✘
+tide_terraform_icon
+tide_time_format %T
+tide_toolbox_icon ⬢
+tide_virtual_env_icon 
--- functions/tide/configure/configs/lean.fish
+++ functions/tide/configure/configs/lean.fish
@@ -1,104 +1,60 @@
-tide_character_color $_tide_color_green
-tide_character_color_failure FF0000
-tide_character_icon ❯
-tide_character_vi_icon_default ❮
-tide_character_vi_icon_replace ▶
-tide_character_vi_icon_visual V
 tide_chruby_bg_color normal
-tide_chruby_color B31209
-tide_chruby_icon 
+tide_chruby_color $_chruby
 tide_cmd_duration_bg_color normal
-tide_cmd_duration_color 87875F
-tide_cmd_duration_decimals 0
-tide_cmd_duration_icon
-tide_cmd_duration_threshold 3000
-tide_context_always_display false
+tide_cmd_duration_color $_dark_yellowish_gray
 tide_context_bg_color normal
-tide_context_color_default D7AF87
-tide_context_color_root $_tide_color_gold
-tide_context_color_ssh D7AF87
 tide_git_bg_color normal
 tide_git_bg_color_unstable normal
 tide_git_bg_color_urgent normal
-tide_git_color_branch $_tide_color_green
-tide_git_color_conflicted FF0000
-tide_git_color_dirty $_tide_color_gold
-tide_git_color_operation FF0000
-tide_git_color_staged $_tide_color_gold
-tide_git_color_stash $_tide_color_green
-tide_git_color_untracked $_tide_color_light_blue
-tide_git_color_upstream $_tide_color_green
-tide_git_icon
-tide_git_truncation_length 24
+tide_git_color_branch $_light_green
+tide_git_color_conflicted $_light_red
+tide_git_color_dirty $_gold
+tide_git_color_operation $_light_red
+tide_git_color_staged $_gold
+tide_git_color_stash $_light_green
+tide_git_color_untracked $_light_blue
+tide_git_color_upstream $_light_green
 tide_go_bg_color normal
-tide_go_color 00ACD7
-tide_go_icon 
+tide_go_color $_go
 tide_jobs_bg_color normal
-tide_jobs_color $_tide_color_dark_green
-tide_jobs_icon 
+tide_jobs_color $_green
 tide_kubectl_bg_color normal
-tide_kubectl_color 326CE5
-tide_kubectl_icon ⎈
+tide_kubectl_color $_kubectl
 tide_left_prompt_frame_enabled false
 tide_left_prompt_items pwd git newline character
-tide_left_prompt_prefix ''
 tide_left_prompt_separator_diff_color ' '
 tide_left_prompt_separator_same_color ' '
 tide_left_prompt_suffix ' '
 tide_node_bg_color normal
-tide_node_color 44883E
-tide_node_icon ⬢
+tide_node_color $_node
 tide_os_bg_color normal
-tide_os_color normal
-tide_os_icon (_tide_detect_os)
+tide_os_color $_subtle_offwhite
 tide_php_bg_color normal
-tide_php_color 617CBE
-tide_php_icon 
+tide_php_color $_php
 tide_private_mode_bg_color normal
-tide_private_mode_color FFFFFF
-tide_private_mode_icon 﫸
-tide_prompt_add_newline_before true
-tide_prompt_color_frame_and_connection 6C6C6C
-tide_prompt_color_separator_same_color 949494
-tide_prompt_icon_connection ' '
-tide_prompt_min_cols 26
+tide_private_mode_color $_pure_white
 tide_prompt_pad_items false
 tide_pwd_bg_color normal
-tide_pwd_color_anchors $_tide_color_light_blue
-tide_pwd_color_dirs $_tide_color_dark_blue
-tide_pwd_color_truncated_dirs 8787AF
-tide_pwd_icon
-tide_pwd_icon_home
-tide_pwd_icon_unwritable 
-tide_pwd_markers .bzr .citc .git .hg .node-version .python-version .ruby-version .shorten_folder_marker .svn .terraform Cargo.toml composer.json CVS go.mod package.json
+tide_pwd_color_anchors $_light_blue
+tide_pwd_color_dirs $_dark_blue
+tide_pwd_color_truncated_dirs $_violet
 tide_right_prompt_frame_enabled false
-tide_right_prompt_items status cmd_duration context jobs node virtual_env rustc php chruby go kubectl toolbox terraform
 tide_right_prompt_prefix ' '
 tide_right_prompt_separator_diff_color ' '
 tide_right_prompt_separator_same_color ' '
-tide_right_prompt_suffix ''
 tide_rustc_bg_color normal
-tide_rustc_color F74C00
-tide_rustc_icon 
+tide_rustc_color $_rustc
 tide_shlvl_bg_color normal
-tide_shlvl_color d78700
-tide_shlvl_icon 
-tide_shlvl_threshold 1
+tide_shlvl_color $_shlvl
 tide_status_bg_color normal
 tide_status_bg_color_failure normal
-tide_status_color $_tide_color_dark_green
-tide_status_color_failure D70000
-tide_status_icon ✔
-tide_status_icon_failure ✘
+tide_status_color_failure $_slightly_dark_red
 tide_terraform_bg_color normal
-tide_terraform_color 844FBA
-tide_terraform_icon
+tide_terraform_color $_terraform
 tide_time_bg_color normal
-tide_time_color 5F8787
-tide_time_format %T
+tide_time_color $_bluish_grayish
 tide_toolbox_bg_color normal
-tide_toolbox_color 613583
-tide_toolbox_icon ⬢
+tide_toolbox_color $_toolbox
 tide_vi_mode_bg_color_default
 tide_vi_mode_bg_color_replace
 tide_vi_mode_bg_color_visual
@@ -109,5 +65,4 @@ tide_vi_mode_icon_default
 tide_vi_mode_icon_replace
 tide_vi_mode_icon_visual
 tide_virtual_env_bg_color normal
-tide_virtual_env_color 00AFAF
-tide_virtual_env_icon 
+tide_virtual_env_color $_virtual_env
