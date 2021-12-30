ae51abe472c458dea910986b320b0905173e41cc
Ilan Cosman <ilancosman@gmail.com>
2021-12-30 08:55:26 -0800

    Work

---
+++ functions/tide/configure/configs/all/meta_16color.fish
@@ -0,0 +1,40 @@
+_almost_white_gray white
+_bluish_grayish cyan
+_chruby red
+_dark_blue brblue
+_dark_gray black | Remember the rules for black!
+_dark_green green
+_dark_yellowish_gray green
+_deep_blue black | Remember the rules for black!
+_go brblue
+_gold yellow
+_gray brblack
+_green green
+_kubectl blue
+_leafy_green green
+_light_blue brblue
+_light_gray brblue
+_light_green brgreen
+_light_red brred
+_node green
+_ochre green
+_orange yellow
+_php blue
+_pure_white brwhite
+_rainbow_fg black | Remember the rules for black!
+_rustc brred
+_shlvl brwhite
+_slightly_dark_red red
+_ssh yellow
+_subtle_offwhite brwhite
+_subtle_offwhite_slightly_different brwhite
+_tango_black black | Remember the rules for black!
+_tango_blue blue
+_tango_white white
+_tango_yellow yellow
+_terraform magenta
+_toolbox magenta
+_violet brmagenta
+_violety_blue brblue
+_virtual_env cyan
+_yellow bryellow
