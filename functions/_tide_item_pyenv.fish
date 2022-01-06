b919a48976e20a69d41d31e041d0b9102c05974e
Yonatan Striem-Amit <yonatan@cybereason.com>
2022-01-06 09:13:30 -0500

    retab

--- functions/_tide_item_pyenv.fish
+++ functions/_tide_item_pyenv.fish
@@ -1,10 +1,10 @@
 function _tide_item_pyenv
-	if test -n "$PYENV_VERSION" \
-		-o -f .python-version \
-		-o -f requirements.txt \
-		-o -f pyproject.toml \
-		-o (count *.py) -gt 0
+    if test -n "$PYENV_VERSION" \
+        -o -f .python-version \
+        -o -f requirements.txt \
+        -o -f pyproject.toml \
+        -o (count *.py) -gt 0
         set -l pyenv_status (pyenv version-name 2>/dev/null) 
         _tide_print_item pyenv $tide_pyenv_icon' ' $pyenv_status 
-	end
+    end
 end
