ccabacb6296c6a6f98c171ae52503d0c9757b774
Ilan Cosman <ilancosman@gmail.com>
2022-03-14 08:43:24 -0700

    Use read instead of command substitution

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -9,8 +9,8 @@ source (functions --details _tide_pwd)
 set -l prompt_var _tide_prompt_$fish_pid
 set -U $prompt_var # Set var here so if we erase $prompt_var, bg job won't set a uvar
 
-set -l color_normal (set_color normal)
-set -l fish_path (status fish-path)
+set_color normal | read -l color_normal
+status fish-path | read -l fish_path
 
 # _tide_repaint prevents us from creating a second background job
 function _tide_refresh_prompt --on-variable $prompt_var --on-variable COLUMNS
