45690162cf5d89128f949c370102a63467bec3ee
Oskar <github@oskar.lol>
2022-03-18 00:45:31 +0100

    Don't omit suffixes in 2-line prompts

--- functions/_tide_2_line_prompt.fish
+++ functions/_tide_2_line_prompt.fish
@@ -3,12 +3,20 @@ function _tide_2_line_prompt
     _tide_side=left for item in $_tide_left_items
         _tide_item_$item
     end
+    if not set -e add_prefix
+        set_color $prev_bg_color -b normal
+        echo $tide_left_prompt_suffix
+    end
 
     echo
 
     set -g add_prefix
     _tide_side=right for item in $_tide_right_items
         _tide_item_$item
+    end
+    if not set -e add_prefix
+        set_color $prev_bg_color -b normal
+        echo $tide_right_prompt_suffix
     end
 end
 
