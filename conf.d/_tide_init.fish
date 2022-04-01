e203fa2ecf4015c41dfa362da8554c0a330f48cd
Ilan Cosman <ilancosman@gmail.com>
2022-04-01 07:58:32 -0700

    Warn users against installing from main

--- conf.d/_tide_init.fish
+++ conf.d/_tide_init.fish
@@ -5,11 +5,20 @@ function _tide_init_install --on-event _tide_init_install
     _load_config lean
     _tide_finish
 
-    status is-interactive && switch (read --prompt-str="Configure tide prompt? [Y/n] " | string lower)
-        case y ye yes ''
-            tide configure
-        case '*'
-            echo -s \n 'Run ' (echo -ns "tide configure" | fish_indent --ansi) ' to customize your prompt.'
+    if status is-interactive
+        if contains ilancosman/tide (string lower $_fisher_plugins)
+            set_color bryellow
+            echo "ilancosman/tide is a development branch. Please install from a release tag:"
+            echo -ns "fisher install ilancosman/tide@v5" | fish_indent --ansi
+            sleep 3
+        end
+
+        switch (read --prompt-str="Configure tide prompt? [Y/n] " | string lower)
+            case y ye yes ''
+                tide configure
+            case '*'
+                echo -s \n 'Run ' (echo -ns "tide configure" | fish_indent --ansi) ' to customize your prompt.'
+        end
     end
 end
 
