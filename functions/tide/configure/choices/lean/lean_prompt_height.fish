dbb8a6e7e384885d3730660e95650a1cd72cfbce
Ilan Cosman <ilancosman@gmail.com>
2022-04-03 13:25:37 -0700

    Fix extra space on single line lean (fixes #274)

--- functions/tide/configure/choices/lean/lean_prompt_height.fish
+++ functions/tide/configure/choices/lean/lean_prompt_height.fish
@@ -3,20 +3,24 @@ function lean_prompt_height
 
     _tide_option 1 'One line'
     _tide_find_and_remove newline fake_tide_left_prompt_items
+    set -g fake_tide_left_prompt_suffix ''
     _tide_display_prompt
 
     _tide_option 2 'Two lines'
     set -g fake_tide_left_prompt_items $fake_tide_left_prompt_items[1..-2] newline $fake_tide_left_prompt_items[-1]
+    set -g fake_tide_left_prompt_suffix ' '
     _tide_display_prompt
 
     _tide_menu
     switch $_tide_selected_option
         case 1
             _tide_find_and_remove newline fake_tide_left_prompt_items
+            set fake_tide_left_prompt_suffix ''
             _next_choice all/prompt_connection_andor_frame_color
         case 2
             _tide_find_and_remove newline fake_tide_left_prompt_items
             set -g fake_tide_left_prompt_items $fake_tide_left_prompt_items[1..-2] newline $fake_tide_left_prompt_items[-1]
+            set -g fake_tide_left_prompt_suffix ' '
             _next_choice all/prompt_connection
     end
 end
