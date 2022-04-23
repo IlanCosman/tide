26f1a8d5720755e4f4bfb29a004dbb65074f25de
Austin Ziegler <austin@zieglers.ca>
2022-04-23 18:47:05 -0400

    Support multiple 'default' contexts for Docker (#283)
    * Support multiple 'default' contexts for Docker
    
    If someone is using 'colima' instead of Docker Desktop, the default
    context is 'colima'. Rather than making two tests with fixed values,
    add a new configuration variable and use `contains`.
    
    * Remove non-colour configuration from color files

--- functions/_tide_item_docker.fish
+++ functions/_tide_item_docker.fish
@@ -1,4 +1,5 @@
 function _tide_item_docker
     docker context inspect --format '{{.Name}}' | read -l context
-    test "$context" != default && _tide_print_item docker $tide_docker_icon' ' $context
+    contains -- "$context" $tide_docker_default_contexts ||
+        _tide_print_item docker $tide_docker_icon' ' $context
 end
--- tests/_tide_item_docker.test.fish
+++ tests/_tide_item_docker.test.fish
@@ -9,5 +9,12 @@ set -lx tide_docker_icon 
 mock docker "context inspect" "printf default"
 _docker # CHECK:
 
+mock docker "context inspect" "printf colima"
+_docker # CHECK:
+
 mock docker "context inspect" "printf curr-context"
 _docker # CHECK:  curr-context
+
+mock docker "context inspect" "printf curr-context"
+set -lx tide_docker_default_contexts curr-context
+_docker # CHECK:
