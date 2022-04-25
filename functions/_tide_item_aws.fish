f59e0ae6e6cf1935111dd08a0ef500ec7faddef6
Ilan Cosman <ilancosman@gmail.com>
2022-04-25 10:21:28 -0700

    Minor edits to item code

--- functions/_tide_item_aws.fish
+++ functions/_tide_item_aws.fish
@@ -1,14 +1,11 @@
 function _tide_item_aws
-    # AWS_PROFILE overrides AWS_DEFAULT_PROFILE
-    set -q AWS_DEFAULT_PROFILE && set -f _profile_name $AWS_DEFAULT_PROFILE
-    set -q AWS_PROFILE && set -f _profile_name $AWS_PROFILE
-    # AWS_REGION overrides AWS_DEFAULT_REGION
-    set -q AWS_DEFAULT_REGION && set -f _region_name $AWS_DEFAULT_REGION
-    set -q AWS_REGION && set -f _region_name $AWS_REGION
+    # AWS_PROFILE overrides AWS_DEFAULT_PROFILE, AWS_REGION overrides AWS_DEFAULT_REGION
+    set -q AWS_PROFILE && set -l AWS_DEFAULT_PROFILE $AWS_PROFILE
+    set -q AWS_REGION && set -l AWS_DEFAULT_REGION $AWS_REGION
 
-    if test -n "$_profile_name" && test -n "$_region_name"
-        _tide_print_item aws $tide_aws_icon' ' "$_profile_name"/"$_region_name"
-    else if test -n "$_profile_name" || test -n "$_region_name"
-        _tide_print_item aws $tide_aws_icon' ' "$_profile_name""$_region_name"
+    if test -n "$AWS_DEFAULT_PROFILE" && test -n "$AWS_DEFAULT_REGION"
+        _tide_print_item aws $tide_aws_icon' ' "$AWS_DEFAULT_PROFILE/$AWS_DEFAULT_REGION"
+    else if test -n "$AWS_DEFAULT_PROFILE$AWS_DEFAULT_REGION"
+        _tide_print_item aws $tide_aws_icon' ' "$AWS_DEFAULT_PROFILE$AWS_DEFAULT_REGION"
     end
 end
