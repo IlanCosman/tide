9ce33d749216b28d2b84f2f58148eaa0e4b51b43
Ilan Cosman <ilancosman@gmail.com>
2022-04-25 10:28:13 -0700

    Fix test for new icon, add aws to list of tool items

--- tests/_tide_item_aws.test.fish
+++ tests/_tide_item_aws.test.fish
@@ -9,19 +9,19 @@ _aws # CHECK:
 set -lx tide_aws_icon 
 set -lx AWS_DEFAULT_PROFILE default_profile
 
-_aws # CHECK: a default_profile
+_aws # CHECK:  default_profile
 
 set -lx AWS_DEFAULT_REGION default_region
 
-_aws # CHECK: a default_profile/default_region
+_aws # CHECK:  default_profile/default_region
 
 set -lx AWS_PROFILE non_default_profile
 set -lx AWS_REGION non_default_region
 
-_aws # CHECK: a non_default_profile/non_default_region
+_aws # CHECK:  non_default_profile/non_default_region
 
 set -e AWS_REGION
 set -e AWS_PROFILE
 set -e AWS_DEFAULT_PROFILE
 
-_aws # CHECK: a default_region
+_aws # CHECK:  default_region
