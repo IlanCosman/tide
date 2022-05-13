5a4af0e812563078b853b8636ff448f8f02a9240
Ilan Cosman <ilancosman@gmail.com>
2022-05-12 18:29:28 -0700

    Make wizard reflect new pwd highlighting

--- functions/tide/configure/functions/_fake_tide_pwd.fish
+++ functions/tide/configure/functions/_fake_tide_pwd.fish
@@ -1,7 +1,6 @@
 function _fake_tide_pwd
     set -g _tide_pwd_len 5
-    echo -ns (set_color $fake_tide_pwd_color_dirs)$fake_tide_pwd_icon' ' \
-        (set_color -o $fake_tide_pwd_color_anchors)'~'(set_color normal -b $fake_tide_pwd_bg_color) \
-        (set_color $fake_tide_pwd_color_dirs)'/'(set_color normal -b $fake_tide_pwd_bg_color) \
+    set_color $fake_tide_pwd_color_dirs
+    echo -ns $fake_tide_pwd_icon' ' '~/' \
         (set_color -o $fake_tide_pwd_color_anchors)'src'(set_color normal -b $fake_tide_pwd_bg_color)
 end
