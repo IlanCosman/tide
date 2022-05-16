2b6a547330bbec4d04383886bfdb1d8895bc33ca
Ilan Cosman <ilancosman@gmail.com>
2022-05-16 07:56:57 -0700

    Minor edits to java item

--- functions/_tide_item_java.fish
+++ functions/_tide_item_java.fish
@@ -1,3 +1,3 @@
 function _tide_item_java
-    test -e pom.xml && _tide_print_item java $tide_java_icon' ' (java -version 2>&1 | string match -r "[\d.]+")[2]
+    test -e pom.xml && _tide_print_item java $tide_java_icon' ' (java -version &| string match -r "[\d.]+")[1]
 end
