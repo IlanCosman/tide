e82e012cf00efbae3e09ccd7e56de25885aa2a62
Next Alone <12210746+NextAlone@users.noreply.github.com>
2022-06-16 15:30:10 +0800

    fix: failed when fish updated

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -10,7 +10,8 @@ set -l prompt_var _tide_prompt_$fish_pid
 set -U $prompt_var # Set var here so if we erase $prompt_var, bg job won't set a uvar
 
 set_color normal | read -l color_normal
-status fish-path | read -l fish_path
+status fish-path | read -l fish_path -a
+set -l fish_path $fish_path[1]
 
 # _tide_repaint prevents us from creating a second background job
 function _tide_refresh_prompt --on-variable $prompt_var --on-variable COLUMNS
