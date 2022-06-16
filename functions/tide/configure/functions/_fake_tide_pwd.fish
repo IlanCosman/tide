fd6c1b0b90842b37345dfe045ed0014b54337efb
Ilan Cosman <ilancosman@gmail.com>
2022-06-16 09:38:00 -0700

    Fix right prompt being mangled by icons in config menu

--- functions/tide/configure/functions/_fake_tide_pwd.fish
+++ functions/tide/configure/functions/_fake_tide_pwd.fish
@@ -1,6 +1,11 @@
 function _fake_tide_pwd
-    set -g _tide_pwd_len 5
-    set_color $fake_tide_pwd_color_dirs
-    echo -ns $fake_tide_pwd_icon' ' '~/' \
-        (set_color -o $fake_tide_pwd_color_anchors)'src'(set_color normal -b $fake_tide_pwd_bg_color)
+    set -l out (
+        set_color $fake_tide_pwd_color_dirs
+        echo -ns $fake_tide_pwd_icon' ' '~/'
+        set_color -o $fake_tide_pwd_color_anchors
+        echo -ns src
+        set_color normal -b $fake_tide_pwd_bg_color
+    )
+    set -g _tide_pwd_len (string length --visible $out)
+    echo -ns $out
 end
