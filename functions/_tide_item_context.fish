20c2d3d5e73ea22896cd265929a7724c7c3fd4a8
Marcelo Origoni <morigoni@mulesoft.com>
2022-08-06 18:22:01 -0300

    changed condition, to enable host being shown by default

--- functions/_tide_item_context.fish
+++ functions/_tide_item_context.fish
@@ -4,10 +4,10 @@ function _tide_item_context
     else if test "$EUID" = 0
         tide_context_color=$tide_context_color_root _tide_print_item context $USER@$hostname
     else if test "$tide_context_always_display" = true
-        if test "$tide_context_always_display_host" = true
-            tide_context_color=$tide_context_color_default _tide_print_item context $USER@$hostname
-        else
+        if test "$tide_context_show_only_user" = true
             tide_context_color=$tide_context_color_default _tide_print_item context $USER
+        else
+            tide_context_color=$tide_context_color_default _tide_print_item context $USER@$hostname
         end
     end
 end
