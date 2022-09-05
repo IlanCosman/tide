b508a7f52ba88f95b95d460aaa2e614f92a77369
Ilan Cosman <ilancosman@gmail.com>
2022-09-05 14:07:29 -0400

    Add tide_context_hostname_parts

--- functions/_tide_item_context.fish
+++ functions/_tide_item_context.fish
@@ -1,9 +1,15 @@
 function _tide_item_context
     if set -q SSH_TTY
-        tide_context_color=$tide_context_color_ssh _tide_print_item context $USER@$hostname
+        set -lx tide_context_color $tide_context_color_ssh
+        test "$tide_context_hostname_parts" = 0 && _tide_print_item context $USER ||
+            h=(string split . $hostname) _tide_print_item context $USER@(string join . $h[..$tide_context_hostname_parts])
     else if test "$EUID" = 0
-        tide_context_color=$tide_context_color_root _tide_print_item context $USER@$hostname
+        set -lx tide_context_color $tide_context_color_root
+        test "$tide_context_hostname_parts" = 0 && _tide_print_item context $USER ||
+            h=(string split . $hostname) _tide_print_item context $USER@(string join . $h[..$tide_context_hostname_parts])
     else if test "$tide_context_always_display" = true
-        tide_context_color=$tide_context_color_default _tide_print_item context $USER@$hostname
+        set -lx tide_context_color $tide_context_color_default
+        test "$tide_context_hostname_parts" = 0 && _tide_print_item context $USER ||
+            h=(string split . $hostname) _tide_print_item context $USER@(string join . $h[..$tide_context_hostname_parts])
     end
 end
--- tests/_tide_item_context.test.fish
+++ tests/_tide_item_context.test.fish
@@ -5,14 +5,19 @@ function _context
     _tide_decolor (_tide_item_context)
 end
 
+set -lx tide_context_hostname_parts 5
+
 set -lx tide_context_always_display false
 _context # Check:
 
 set -lx tide_context_always_display true
-_context # CHECK: {{.*@.*}}
+_context # CHECK: {{[^@]*@[^@]*}}
 
 set -g SSH_TTY /dev/pts/0
-_context # CHECK: {{.*@.*}}
+_context # CHECK: {{[^@]*@[^@]*}}
 
 set EUID 0
-_context # CHECK: {{.*@.*}}
+_context # CHECK: {{[^@]*@[^@]*}}
+
+set -lx tide_context_hostname_parts 0
+_context # CHECK: {{[^@]*}}
