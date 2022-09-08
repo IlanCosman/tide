73bd2d13c4ce51b2989821fc181132beccbd967c
jazzsnobeatcake <85142501+jazzsnobeatcake@users.noreply.github.com>
2022-09-08 06:09:23 +0200

    Display toolbox name
    Displays the toolbox name instead of the word toolbox.
    $tide_toolbox_icon already indicates we're in a toolbox, displaying the toolbox name adds further context in multi-toolbox environments.

--- functions/_tide_item_toolbox.fish
+++ functions/_tide_item_toolbox.fish
@@ -1,3 +1,3 @@
 function _tide_item_toolbox
-    test -e /run/.toolboxenv && _tide_print_item toolbox $tide_toolbox_icon' ' $hostname
+    test -e /run/.toolboxenv && _tide_print_item toolbox $tide_toolbox_icon' ' $(cat /run/.containerenv | sed -n '2 p' | awk -F '"' 'NF>2{print $2}')
 end
