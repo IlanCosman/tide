b0ade0fe504be4a45cae33f3ad0feea5c73476af
Devansh Sharma <devansh.sharma@gmail.com>
2022-09-18 14:04:30 +0530

    Replace python script with sha1sum command; Update to use fish builtins

--- functions/_tide_item_pulumi.fish
+++ functions/_tide_item_pulumi.fish
@@ -1,10 +1,23 @@
 function _tide_item_pulumi
     if test -e Pulumi.yaml
-        set -l project_name (grep name Pulumi.yaml | string split -f 2 ' ')
+        set -l project_name (string match -rg 'name: ([^ ]+)' < Pulumi.yaml)
         set -l yaml_path (pwd)/Pulumi.yaml
-        set -l path_hash (python -c "import hashlib; print(hashlib.sha1(b'$yaml_path').hexdigest())")
-        set -l stack (cat ~/.pulumi/workspaces/$project_name-$path_hash-workspace.json | grep stack | string split -f 4 '"')
 
-        _tide_print_item pulumi $tide_pulumi_icon' ' (echo $stack)
+        set -l path_hash ''
+        if command -q sha1sum
+            set path_hash (echo -n "$yaml_path" | sha1sum | string sub -e 40)
+        else if command -q shasum
+            set path_hash (echo -n "$yaml_path" | shasum | string sub -e 40)
+        end
+
+        if test -n "$path_hash"
+            set -l workspace_file "$HOME/.pulumi/workspaces/$project_name-$path_hash-workspace.json"
+
+            if test -e $workspace_file
+                set -l stack (string match -rg '[\'|"]stack[\'|"]:[ ]{0,1}[\'|"]([^"\']+)[\'|"]' < $workspace_file)
+
+                _tide_print_item pulumi $tide_pulumi_icon' ' (echo $stack)
+            end
+        end
     end
 end
--- tests/_tide_item_pulumi.test.fish
+++ tests/_tide_item_pulumi.test.fish
@@ -7,16 +7,33 @@ end
 set -l tmpdir (mktemp -d)
 cd $tmpdir
 
-# No Pulumi.yaml file
+# Test #1: No Pulumi.yaml file
 _pulumi # CHECK:
 
-# Pulumi.yaml file with sample project name
+# Test #2: No workspace file
+# Setting up sample Pulumi.yaml file
 touch Pulumi.yaml
 echo "name: pulumi-test" >Pulumi.yaml
 
-# Mock contents of workspace json with sample stack
-mock cat \* "echo '{\"stack\": \"dev\"}'"
+# Setting up sample workspaces folder
+mkdir -p .pulumi/workspaces/
 
-_pulumi # CHECK:  dev
+# Calculating path hash for sample Pulumi file in $tmpdir
+set -l mock_path_hash ''
+if command -q sha1sum
+    set mock_path_hash (echo -n "$tmpdir/Pulumi.yaml" | sha1sum | string sub -e 40)
+else if command -q shasum
+    set mock_path_hash (echo -n "$tmpdir/Pulumi.yaml" | shasum | string sub -e 40)
+end
+
+# Overriding $HOME to force use sample workspace file
+HOME=$tmpdir _pulumi # CHECK:
+
+# Test #3: Ideal path
+# Setting up sample workspace file with 'dev' as current stack
+echo "{\"stack\": \"dev\"}" >.pulumi/workspaces/pulumi-test-$mock_path_hash-workspace.json
+
+# Overriding $HOME to force use sample workspace file
+HOME=$tmpdir _pulumi # CHECK:  dev
 
 command rm -r $tmpdir
