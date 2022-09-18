748eceeafc773ab319b064056344197bb7d1ddcb
Ilan Cosman <ilancosman@gmail.com>
2022-09-18 12:32:22 -0700

    Some changes to pulumi item

--- functions/_tide_item_pulumi.fish
+++ functions/_tide_item_pulumi.fish
@@ -1,22 +1,18 @@
 function _tide_item_pulumi
     if test -e Pulumi.yaml
-        set -l project_name (string match -rg 'name: ([^ ]+)' < Pulumi.yaml)
-        set -l yaml_path (pwd)/Pulumi.yaml
-
-        set -l path_hash ''
         if command -q sha1sum
-            set path_hash (echo -n "$yaml_path" | sha1sum | string sub -e 40)
+            echo -n "$PWD/Pulumi.yaml" | sha1sum | string sub -e40 | read -f path_hash
         else if command -q shasum
-            set path_hash (echo -n "$yaml_path" | shasum | string sub -e 40)
+            echo -n "$PWD/Pulumi.yaml" | shasum | string sub -e40 | read -f path_hash
         end
 
         if test -n "$path_hash"
+            string match -rg 'name: *(.*)' <Pulumi.yaml | read -l project_name
             set -l workspace_file "$HOME/.pulumi/workspaces/$project_name-$path_hash-workspace.json"
 
             if test -e $workspace_file
-                set -l stack (string match -rg '[\'|"]stack[\'|"]:[ ]{0,1}[\'|"]([^"\']+)[\'|"]' < $workspace_file)
-
-                _tide_print_item pulumi $tide_pulumi_icon' ' (echo $stack)
+                string match -rg '"stack": *"(.*)"' <$workspace_file | read -l stack
+                _tide_print_item pulumi $tide_pulumi_icon' ' $stack
             end
         end
     end
