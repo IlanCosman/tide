e8b2443cd7b65f8269b3fcf0377a5870a957bd47
jazzsnobeatcake <85142501+jazzsnobeatcake@users.noreply.github.com>
2022-09-19 22:53:20 +0200

    Display toolbox name (#343)
    * Display toolbox name 
    
    Displays the toolbox name instead of the word toolbox.
    $tide_toolbox_icon already indicates we're in a toolbox, displaying the toolbox name adds further context in multi-toolbox environments.
    
    * Use string match and redirection
    
    Co-authored-by: Ilan Cosman <ilancosman@gmail.com>

--- functions/_tide_item_toolbox.fish
+++ functions/_tide_item_toolbox.fish
@@ -1,3 +1,4 @@
 function _tide_item_toolbox
-    test -e /run/.toolboxenv && _tide_print_item toolbox $tide_toolbox_icon' ' $hostname
+    test -e /run/.toolboxenv &&
+        _tide_print_item toolbox $tide_toolbox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
 end
