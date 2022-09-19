15494c823e38232a0ac55d7da3d668523f227484
Ilan Cosman <ilancosman@gmail.com>
2022-09-19 13:30:28 -0700

    Use string match and redirection

--- functions/_tide_item_toolbox.fish
+++ functions/_tide_item_toolbox.fish
@@ -1,3 +1,4 @@
 function _tide_item_toolbox
-    test -e /run/.toolboxenv && _tide_print_item toolbox $tide_toolbox_icon' ' $(cat /run/.containerenv | sed -n '2 p' | awk -F '"' 'NF>2{print $2}')
+    test -e /run/.toolboxenv &&
+        _tide_print_item toolbox $tide_toolbox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
 end
