2a79f03b2ab379d713f0940b27f501754953f51c
Taha <62564400+mt190502@users.noreply.github.com>
2022-09-29 09:58:38 +0300

    Fix distrobox prompt support

--- functions/_tide_item_distrobox.fish
+++ functions/_tide_item_distrobox.fish
@@ -1,4 +1,7 @@
 function _tide_item_distrobox
-    test -e /run/.distroboxenv &&
-        _tide_print_item distrobox $tide_distrobox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
+    if not command -v distrobox-enter >/dev/null
+        if command -v distrobox-export >/dev/null
+            _tide_print_item distrobox $tide_distrobox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
+		end
+	end
 end
