76115ade63bbb84d9728465d8fd75e84bca44ea4
Ilan Cosman <ilancosman@gmail.com>
2022-09-29 19:44:20 -0700

    Simplify and format

--- functions/_tide_item_distrobox.fish
+++ functions/_tide_item_distrobox.fish
@@ -1,7 +1,4 @@
 function _tide_item_distrobox
-    if not command -v distrobox-enter >/dev/null
-        if command -v distrobox-export >/dev/null
-            _tide_print_item distrobox $tide_distrobox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
-		end
-	end
+    command -q distrobox-enter || # distrobox-enter is not present in the container
+        _tide_print_item distrobox $tide_distrobox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
 end
--- functions/_tide_remove_unusable_items.fish
+++ functions/_tide_remove_unusable_items.fish
@@ -4,8 +4,8 @@ function _tide_remove_unusable_items
     for item in aws chruby crystal distrobox docker git go java kubectl nix_shell node php pulumi rustc terraform toolbox virtual_env
         set -l cli_names $item
         switch $item
-            case distrobox                           # there is no '/usr/bin/distrobox' inside the container. 'distrobox-export' and 'distrobox-host-exec' are found. 
-                set cli_names distrobox-export       # It would be more accurate to check the existence of these two files. See: https://github.com/IlanCosman/tide/pull/351
+            case distrobox # there is no 'distrobox' command inside the container
+                set cli_names distrobox-export # 'distrobox-export' and 'distrobox-host-exec' are available
             case virtual_env
                 set cli_names python python3
             case nix_shell
