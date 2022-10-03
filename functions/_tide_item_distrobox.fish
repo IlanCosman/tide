d61e99ffcb28409993ba19181c81598c7b325177
Taha <62564400+mt190502@users.noreply.github.com>
2022-10-03 22:10:52 +0300

    Fix distrobox prompt support

--- functions/_tide_item_distrobox.fish
+++ functions/_tide_item_distrobox.fish
@@ -1,4 +1,4 @@
 function _tide_item_distrobox
-    command -q distrobox-enter || # distrobox-enter is not present in the container
+    test -e /etc/profile.d/distrobox_profile.sh && test -e /run/.containerenv &&                          # distrobox-enter is not present in the container
         _tide_print_item distrobox $tide_distrobox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
 end
