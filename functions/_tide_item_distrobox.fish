be19bfef58e60e76eb378a34e6d6e9b8fed29d99
Ilan Cosman <ilancosman@gmail.com>
2022-10-14 14:03:04 -0700

    Remove extraneous command

--- functions/_tide_item_distrobox.fish
+++ functions/_tide_item_distrobox.fish
@@ -1,4 +1,4 @@
 function _tide_item_distrobox
-    test -e /etc/profile.d/distrobox_profile.sh && test -e /run/.containerenv &&                          # distrobox-enter is not present in the container
+    test -e /etc/profile.d/distrobox_profile.sh && test -e /run/.containerenv &&
         _tide_print_item distrobox $tide_distrobox_icon' ' (string match -rg 'name="(.*)"' </run/.containerenv)
 end
