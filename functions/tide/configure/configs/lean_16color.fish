9a1e94c4b6d5591d2886ec42bb171c9211092705
Ilan Cosman <ilancosman@gmail.com>
2023-03-25 12:20:05 -0700

    Add item to list, update color, add to 16_color configs

--- functions/tide/configure/configs/lean_16color.fish
+++ functions/tide/configure/configs/lean_16color.fish
@@ -12,10 +12,12 @@ tide_context_color_root bryellow
 tide_context_color_ssh yellow
 tide_crystal_bg_color normal
 tide_crystal_color brwhite
-tide_docker_bg_color normal
-tide_docker_color blue
 tide_distrobox_bg_color normal
 tide_distrobox_color brmagenta
+tide_docker_bg_color normal
+tide_docker_color blue
+tide_elixir_bg_color normal
+tide_elixir_color magenta
 tide_git_bg_color normal
 tide_git_bg_color_unstable normal
 tide_git_bg_color_urgent normal
