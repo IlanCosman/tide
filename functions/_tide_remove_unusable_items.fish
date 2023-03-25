6e4739afd4ca19458a9eb8ab5d396264e94d57ec
Ilan Cosman <ilancosman@gmail.com>
2023-03-25 12:33:02 -0700

    autoremove elixir item

--- functions/_tide_remove_unusable_items.fish
+++ functions/_tide_remove_unusable_items.fish
@@ -1,7 +1,7 @@
 function _tide_remove_unusable_items
     # Remove tool-specific items for tools the machine doesn't have installed
     set -l removed_items
-    for item in aws chruby crystal distrobox docker git go java kubectl nix_shell node php pulumi rustc terraform toolbox virtual_env
+    for item in aws chruby crystal distrobox docker elixir git go java kubectl nix_shell node php pulumi rustc terraform toolbox virtual_env
         set -l cli_names $item
         switch $item
             case distrobox # there is no 'distrobox' command inside the container
