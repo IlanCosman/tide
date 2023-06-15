ada8bb1b0118bf44faf3103f8df9c5d80068b981
Bazyli Cyran <bazyli.cyran@nokia.com>
2023-06-15 19:54:42 +0200

    Display Python version in virtual_env item

--- tests/_tide_item_virtual_env.test.fish
+++ tests/_tide_item_virtual_env.test.fish
@@ -5,16 +5,17 @@ function _virtual_env
     _tide_decolor (_tide_item_virtual_env)
 end
 
+mock python --version "echo Python 3.11.3"
 set -lx tide_virtual_env_icon 
 
 set -lx VIRTUAL_ENV
 _virtual_env # CHECK:
 
 set -lx VIRTUAL_ENV /home/ilan/python_project/non-generic-name
-_virtual_env # CHECK:  non-generic-name
+_virtual_env # CHECK:  3.11.3 (non-generic-name)
 
 set -lx VIRTUAL_ENV /home/ilan/python_project/venv
-_virtual_env # CHECK:  python_project
+_virtual_env # CHECK:  3.11.3 (python_project)
 
 set -lx VIRTUAL_ENV /home/ilan/.local/share/virtualenvs/pipenv_project-EwRYuc3l
-_virtual_env # CHECK:  pipenv_project
+_virtual_env # CHECK:  3.11.3 (pipenv_project)
