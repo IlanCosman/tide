887d3abd0e4a269ccde785655ff263161f63536c
Ilan Cosman <ilancosman@gmail.com>
2023-07-06 21:31:57 -0700

    use string matching in php item

--- functions/_tide_item_php.fish
+++ functions/_tide_item_php.fish
@@ -1,4 +1,6 @@
 function _tide_item_php
-    path is $_tide_parent_dirs/composer.json &&
-        _tide_print_item php $tide_php_icon' ' (php --version | string match -r 'PHP ([\d.]+)')[2]
+    if path is $_tide_parent_dirs/composer.json
+        php --version | string match -qr "(?<ver>[\d.]+)"
+        _tide_print_item php "$tide_php_icon $ver"
+    end
 end
