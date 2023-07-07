fb8e3bb6a34eaf42917c07c446ac4153951d1fd4
Ilan Cosman <ilancosman@gmail.com>
2023-07-06 21:25:43 -0700

    use string matching in java item

--- functions/_tide_item_java.fish
+++ functions/_tide_item_java.fish
@@ -1,4 +1,6 @@
 function _tide_item_java
-    path is $_tide_parent_dirs/pom.xml &&
-        _tide_print_item java $tide_java_icon' ' (java -version &| string match -r "[\d.]+")[1]
+    if path is $_tide_parent_dirs/pom.xml
+        java -version &| string match -qr "(?<ver>[\d.]+)"
+        _tide_print_item java "$tide_java_icon $ver"
+    end
 end
