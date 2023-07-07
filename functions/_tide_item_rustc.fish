8db7142d0bb4b2fc596538973332ce95b7c22fea
Ilan Cosman <ilancosman@gmail.com>
2023-07-06 21:32:51 -0700

    use string matching in rustc item

--- functions/_tide_item_rustc.fish
+++ functions/_tide_item_rustc.fish
@@ -1,4 +1,6 @@
 function _tide_item_rustc
-    path is $_tide_parent_dirs/Cargo.toml &&
-        _tide_print_item rustc $tide_rustc_icon' ' (rustc --version | string split ' ')[2]
+    if path is $_tide_parent_dirs/Cargo.toml
+        rustc --version | string match -qr "(?<ver>[\d.]+)"
+        _tide_print_item rustc "$tide_rustc_icon $ver"
+    end
 end
