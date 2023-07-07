d7f5041617ca1eb53132293b5671cd4d9aef92df
Ilan Cosman <ilancosman@gmail.com>
2023-07-06 21:07:08 -0700

    use string matching in node item

--- functions/_tide_item_node.fish
+++ functions/_tide_item_node.fish
@@ -1,4 +1,6 @@
 function _tide_item_node
-    path is $_tide_parent_dirs/package.json &&
-        _tide_print_item node $tide_node_icon' ' (node --version | string trim --chars=v)
+    if path is $_tide_parent_dirs/package.json
+        node --version | string match -qr "v(?<ver>.*)"
+        _tide_print_item node $tide_node_icon' ' $ver
+    end
 end
