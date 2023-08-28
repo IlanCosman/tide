4bb29f921f32d62bcaca9013fe9a1f0efeb9ba38
Yuntao (Brady) Zhao <yuntaz@proton.me>
2023-08-28 01:00:06 -0700

    fix prompt: Guard against missing 'node' command (#431)
    Check for 'node' command availability before querying its version in the tide prompt. Prevents errors for users without Node.js installed.

--- functions/_tide_item_node.fish
+++ functions/_tide_item_node.fish
@@ -1,5 +1,5 @@
 function _tide_item_node
-    if path is $_tide_parent_dirs/package.json
+    if command -q node && path is $_tide_parent_dirs/package.json
         node --version | string match -qr "v(?<v>.*)"
         _tide_print_item node $tide_node_icon' ' $v
     end
