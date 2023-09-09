1e08167a19236fae40dfd4de5d65885b649c9336
Ilan Cosman <ilancosman@gmail.com>
2023-09-09 08:24:21 -0700

    Add reload to completions

--- completions/tide.fish
+++ completions/tide.fish
@@ -1,9 +1,10 @@
 complete tide --no-files
 
-set -l subcommands bug-report configure
+set -l subcommands bug-report configure reload
 
 complete tide -x -n __fish_use_subcommand -a bug-report -d "Print info for use in bug reports"
 complete tide -x -n __fish_use_subcommand -a configure -d "Run the configuration wizard"
+complete tide -x -n __fish_use_subcommand -a reload -d "Reload tide configuration"
 
 complete tide -x -n "not __fish_seen_subcommand_from $subcommands" -s h -l help -d "Print help message"
 complete tide -x -n "not __fish_seen_subcommand_from $subcommands" -s v -l version -d "Print tide version"
