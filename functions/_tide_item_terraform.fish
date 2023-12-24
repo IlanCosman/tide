1759ebbae0c17bb9f223755decf18c57302dfba2
Ilan Cosman <ilancosman@gmail.com>
2023-12-24 11:14:27 -0800

    Minor edits to terraform item

--- functions/_tide_item_terraform.fish
+++ functions/_tide_item_terraform.fish
@@ -1,6 +1,5 @@
 function _tide_item_terraform
-    if path is $_tide_parent_dirs/.terraform
-        terraform workspace show | read -l workspace
-        test $workspace != default && _tide_print_item terraform $tide_terraform_icon' ' $workspace
-    end
+    path is $_tide_parent_dirs/.terraform &&
+        terraform workspace show | string match -v default | read -l w &&
+        _tide_print_item terraform $tide_terraform_icon' ' $w
 end
