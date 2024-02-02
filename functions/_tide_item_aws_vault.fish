97565d63f0551b6815607f6d3d8a5397f3c8468b
Mārtiņš Jakubovičs <martins.jakubovics@mintos.com>
2024-02-02 20:18:59 +0200

    fix: format

--- functions/_tide_item_aws_vault.fish
+++ functions/_tide_item_aws_vault.fish
@@ -1,5 +1,5 @@
 function _tide_item_aws_vault
     if test -n "$AWS_VAULT"
-       _tide_print_item aws_vault $tide_aws_vault_icon' ' "$AWS_VAULT"
+        _tide_print_item aws_vault $tide_aws_vault_icon' ' "$AWS_VAULT"
     end
 end
