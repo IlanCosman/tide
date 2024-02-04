f35461a077861b28b35fd9295a00ba355f6d8780
MarcoVeio <marco_a_thomaz@hotmail.com>
2024-02-04 03:16:05 -0300

    testing character with different fish_key_bindings and fish_bind_mode

--- tests/_tide_item_character.test.fish
+++ tests/_tide_item_character.test.fish
@@ -9,6 +9,9 @@ end
 
 set -lx tide_character_icon_failure ✘
 
+# Check icons while fish_key_bindings is fish_default_key_bindings
+set -lx fish_key_bindings fish_default_key_bindings
+
 true
 _character # CHECK: ❯
 
@@ -33,3 +36,145 @@ _character # CHECK: ❯
 
 not false | true
 _character # CHECK: ✘
+
+# Check icons while fish_key_bindings isn't fish_default_key_bindings
+set -lx fish_key_bindings something_else
+# Check icons while fish_bind_mode is default
+set -lx fish_bind_mode default
+
+true
+_character # CHECK: ❮
+
+false
+_character # CHECK: ❮
+
+true | false
+_character # CHECK: ❮
+
+true | true
+_character # CHECK: ❮
+
+false | true
+_character # CHECK: ❮
+
+false | false
+_character # CHECK: ❮
+
+# Check that not command works
+not true | false
+_character # CHECK: ❮
+
+not false | true
+_character # CHECK: ❮
+
+# Check icons while fish_bind_mode is insert
+set -lx fish_bind_mode insert
+
+true
+_character # CHECK: ❯
+
+false
+_character # CHECK: ✘
+
+true | false
+_character # CHECK: ✘
+
+true | true
+_character # CHECK: ❯
+
+false | true
+_character # CHECK: ❯
+
+false | false
+_character # CHECK: ✘
+
+# Check that not command works
+not true | false
+_character # CHECK: ❯
+
+not false | true
+_character # CHECK: ✘
+
+# Check icons while fish_bind_mode is replace
+set -lx fish_bind_mode replace
+
+true
+_character # CHECK: ▶
+
+false
+_character # CHECK: ▶
+
+true | false
+_character # CHECK: ▶
+
+true | true
+_character # CHECK: ▶
+
+false | true
+_character # CHECK: ▶
+
+false | false
+_character # CHECK: ▶
+
+# Check that not command works
+not true | false
+_character # CHECK: ▶
+
+not false | true
+_character # CHECK: ▶
+
+# Check icons while fish_bind_mode is replace_one
+set -lx fish_bind_mode replace_one
+
+true
+_character # CHECK: ▶
+
+false
+_character # CHECK: ▶
+
+true | false
+_character # CHECK: ▶
+
+true | true
+_character # CHECK: ▶
+
+false | true
+_character # CHECK: ▶
+
+false | false
+_character # CHECK: ▶
+
+# Check that not command works
+not true | false
+_character # CHECK: ▶
+
+not false | true
+_character # CHECK: ▶
+
+# Check icons while fish_bind_mode is visual
+set -lx fish_bind_mode visual
+
+true
+_character # CHECK: V
+
+false
+_character # CHECK: V
+
+true | false
+_character # CHECK: V
+
+true | true
+_character # CHECK: V
+
+false | true
+_character # CHECK: V
+
+false | false
+_character # CHECK: V
+
+# Check that not command works
+not true | false
+_character # CHECK: V
+
+not false | true
+_character # CHECK: V
