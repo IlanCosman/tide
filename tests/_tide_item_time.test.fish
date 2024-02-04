cf41dd68f2ade388e48fcd94be9e8636e6cbbfa4
MarcoVeio <marco_a_thomaz@hotmail.com>
2024-02-03 23:05:51 -0300

    fixing tests

--- tests/_tide_item_time.test.fish
+++ tests/_tide_item_time.test.fish
@@ -5,7 +5,7 @@ function _time -a format
     LC_TIME=en_US.UTF-8 _tide_decolor (tide_time_format="$format" _tide_item_time)
 end
 
-set -lx tide_time_icon =
+set -lx tide_time_icon
 
 # None
 _time '' # CHECK:
@@ -16,7 +16,7 @@ _time %T # CHECK: {{\d\d:\d\d:\d\d}}
 # 12 Hour
 _time %r # CHECK: {{\d\d:\d\d:\d\d (A|P)M}}
 
-set -lx tide_time_icon = 
+set -lx tide_time_icon 
 
 # Only Icon
 _time '' # CHECK: 
