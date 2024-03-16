6a693fd92828a437bc9f92ad245ed74692d4fad3
Eugene Oliveros <yujin@jinyuz.dev>
2024-03-16 21:12:47 +0800

    Handle virtualenv names without `-`

--- functions/_tide_item_python.fish
+++ functions/_tide_item_python.fish
@@ -9,7 +9,7 @@ function _tide_item_python
         # pipenv $VIRTUAL_ENV looks like /home/ilan/.local/share/virtualenvs/pipenv_project-EwRYuc3l
         # Detect whether we are using pipenv by looking for 'virtualenvs'. If so, remove the hash at the end.
         if test "$dir" = virtualenvs
-            string match -qr "(?<base>.*)-.*" $base
+            string match -qr "(?<base>[^/-]*)" $base
             _tide_print_item python $tide_python_icon' ' "$v ($base)"
         else if contains -- "$base" virtualenv venv .venv env # avoid generic names
             _tide_print_item python $tide_python_icon' ' "$v ($dir)"
