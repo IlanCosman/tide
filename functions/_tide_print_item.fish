6a96aad4de19e1c9d43b9f9ca69b79fc19754be6
Yang Liu <50459973+ly4096x@users.noreply.github.com>
2025-03-14 03:40:47 -0700

    fix color

--- functions/_tide_print_item.fish
+++ functions/_tide_print_item.fish
@@ -9,7 +9,7 @@ function _tide_print_item -a item
         set_color $item_bg_color -b normal
         v=tide_"$_tide_side"_prompt_prefix echo -ns $$v
     else if test "$item_bg_color" = "$prev_bg_color"
-        v=tide_"$_tide_side"_prompt_separator_same_color echo -ns $tide_prompt_color_separator_same_color$$v
+        v=tide_"$_tide_side"_prompt_separator_same_color echo -ns $_tide_color_separator_same_color$$v
     else if test $_tide_side = left
         set_color $prev_bg_color -b $item_bg_color
         echo -ns $tide_left_prompt_separator_diff_color
