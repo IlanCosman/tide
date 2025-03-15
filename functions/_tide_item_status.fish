f978d994b6a9f865924029b9fef67bf4bf1adefe
Eric Jacobs <ejacobs@doximity.com>
2025-03-14 22:27:34 -0600

    actually print the status value

--- functions/_tide_item_status.fish
+++ functions/_tide_item_status.fish
@@ -6,7 +6,7 @@ function _tide_item_status
 
     if contains "$_tide_pipestatus" $_tide_status_simple_failures
         contains character $_tide_left_items || tide_status_bg_color=$tide_status_bg_color_failure \
-            tide_status_color=$tide_status_color_failure _tide_print_item status $tide_status_icon_failure' ' 1
+            tide_status_color=$tide_status_color_failure _tide_print_item status $tide_status_icon_failure' ' "$_tide_pipestatus"
     else
         fish_status_to_signal $_tide_pipestatus | string replace SIG '' | string join '|' | read -l out
         test $_tide_status = 0 && _tide_print_item status $tide_status_icon' ' $out ||
