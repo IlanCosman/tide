16d8dba77dbec2e53d22e9228c710c7c3c43240f
Nick Nungester <nnungest@gmail.com>
2025-03-25 21:54:24 -0400

    fix: remove hacky length test, clean up join
    credit to @scaryrawr where credit is due.

--- functions/_tide_parent_dirs.fish
+++ functions/_tide_parent_dirs.fish
@@ -2,11 +2,8 @@ function _tide_parent_dirs --on-variable PWD
     set -g _tide_parent_dirs (
         string escape (
             for dir in (string split / -- $PWD)
-                if test (string length \"$dir\") -lt 1
-                    continue
-                end
                 set -fa parts $dir
-                string join "" (string join / --  $parts) ""
+                string join / -- $parts
             end
         )
     )
