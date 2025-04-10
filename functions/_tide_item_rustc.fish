bc4ef80e4f4d53b0245121fdd258ffaaa05f0202
Ehren Bendler <a5ehren@gmail.com>
2025-04-10 11:56:22 -0400

    Update rustc version grabber and test to match current stable output of 'rustup show active-toolchain -v'

--- functions/_tide_item_rustc.fish
+++ functions/_tide_item_rustc.fish
@@ -1,6 +1,6 @@
 function _tide_item_rustc
     if path is $_tide_parent_dirs/Cargo.toml
-        rustup show active-toolchain | string match -qr "(?<v>^[\d.]+)"
+        rustup show active-toolchain -v | string match -qr "rustc (?<v>\d+\.\d+\.\d+)"
         _tide_print_item rustc $tide_rustc_icon' ' $v
     end
 end
--- tests/_tide_item_rustc.test.fish
+++ tests/_tide_item_rustc.test.fish
@@ -8,12 +8,13 @@ end
 set -l rustcDir (mktemp -d)
 cd $rustcDir
 
-mock rustc --version "echo rustc 1.30.0"
+mock rustup \* "echo \"stable-x86_64-unknown-linux-gnu (default)\"; echo \"rustc 1.86.0 (05f9846f8 2025-03-31)\""
+
 set -lx tide_rustc_icon 
 
 _rustc # CHECK:
 
 touch Cargo.toml
-_rustc # CHECK:  1.30.0
+_rustc # CHECK:  1.86.0
 
 command rm -r $rustcDir
