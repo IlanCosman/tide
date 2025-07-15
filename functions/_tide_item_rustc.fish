b083fc4b5f3b51d47b98fbccf4afdd74a7fd746b
Ben Edwards <ben@artfuldodge.io>
2025-07-15 20:28:30 +0100

    Query either rustc or rustup
    Query rustc in preference, falling back to rust up if not present.

--- functions/_tide_item_rustc.fish
+++ functions/_tide_item_rustc.fish
@@ -1,6 +1,8 @@
 function _tide_item_rustc
     if path is $_tide_parent_dirs/Cargo.toml
-        rustup show active-toolchain -v | string match -qr "rustc (?<v>\d+\.\d+\.\d+)"
-        _tide_print_item rustc $tide_rustc_icon' ' $v
+        type -q rustc && set -f cmd 'rustc --version' ||
+        begin type -q rustup && set -f cmd 'rustup show active-toolchain -v'; end &&
+        eval "$cmd" | string match -qr "(?<v>\d+\.\d+\.\d+)" && 
+            _tide_print_item rustc $tide_rustc_icon' ' $v
     end
 end
--- tests/_tide_item_rustc.test.fish
+++ tests/_tide_item_rustc.test.fish
@@ -1,11 +1,17 @@
 # RUN: %fish %s
 _tide_parent_dirs
 
+_tide_item_rustc
+
 function _rustc
     _tide_decolor (_tide_item_rustc)
 end
 
-set -l rustcDir (mktemp -d)
+set rustcDir (mktemp -d)
+function cleanup -p $fish_pid
+    command rm -r $rustcDir
+end
+
 cd $rustcDir
 
 mock rustup \* "echo \"stable-x86_64-unknown-linux-gnu (default)\"; echo \"rustc 1.86.0 (05f9846f8 2025-03-31)\""
@@ -17,4 +23,7 @@ _rustc # CHECK:
 touch Cargo.toml
 _rustc # CHECK:  1.86.0
 
-command rm -r $rustcDir
+mock rustc \* "echo 'rustc 1.85.1 (4eb161250 2025-03-15)'"
+
+touch Cargo.toml
+_rustc # CHECK:  1.85.1
