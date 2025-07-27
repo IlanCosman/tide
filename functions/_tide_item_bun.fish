a45496f62900c243a696d94aaee30255f228e943
Twinkle <saintwinkle@gmail.com>
2025-07-27 11:25:00 +0800

    Update bun lockfile detection
    See: https://bun.com/blog/bun-lock-text-lockfile

--- functions/_tide_item_bun.fish
+++ functions/_tide_item_bun.fish
@@ -1,5 +1,5 @@
 function _tide_item_bun
-    if path is $_tide_parent_dirs/bun.lockb
+    if path is $_tide_parent_dirs/bun.lockb $_tide_parent_dirs/bun.lock
         bun --version | string match -qr "(?<v>.*)"
         _tide_print_item bun $tide_bun_icon' ' $v
     end
