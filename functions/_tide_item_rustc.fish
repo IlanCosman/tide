c21e309555da6325bfc9260c9c4935d0f6a68f4a
Ben Edwards <ben@artfuldodge.io>
2025-08-01 12:12:56 +0100

    Fix ci lints

--- functions/_tide_item_rustc.fish
+++ functions/_tide_item_rustc.fish
@@ -1,8 +1,10 @@
 function _tide_item_rustc
     if path is $_tide_parent_dirs/Cargo.toml
         type -q rustc && set -f cmd 'rustc --version' ||
-        begin type -q rustup && set -f cmd 'rustup show active-toolchain -v'; end &&
-        eval "$cmd" | string match -qr "(?<v>\d+\.\d+\.\d+)" && 
+            begin
+                type -q rustup && set -f cmd 'rustup show active-toolchain -v'
+            end &&
+            eval "$cmd" | string match -qr "(?<v>\d+\.\d+\.\d+)" &&
             _tide_print_item rustc $tide_rustc_icon' ' $v
     end
 end
