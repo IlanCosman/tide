bbcd8062392171fdc39cd0e841efc2dd28c619a3
Twinkle <saintwinkle@gmail.com>
2025-08-04 03:48:27 +0800

    Update bun lockfile detection and use proper bun nerdfont icon (#590)
    * Update bun lockfile detection
    
    See: https://bun.com/blog/bun-lock-text-lockfile
    
    * Update _tide_item_bun.test.fish
    
    * only look for bun.lock, update icon
    
    ---------
    
    Co-authored-by: Ilan Cosman <ilancosman@gmail.com>

--- tests/_tide_item_bun.test.fish
+++ tests/_tide_item_bun.test.fish
@@ -8,12 +8,13 @@ end
 set -l tmpdir (mktemp -d)
 cd $tmpdir
 
-mock bun --version "echo 1.0.24"
-set -lx tide_bun_icon 󰳓
+mock bun --version "echo 1.1.39"
+set -lx tide_bun_icon 
 
 _bun # CHECK:
 
-touch bun.lockb
-_bun # CHECK: 󰳓 1.0.24
+touch bun.lock
+_bun # CHECK:  1.1.39
+command rm bun.lock
 
 command rm -r $tmpdir
